cd8996a5d4816b1ffcc25989b444657864605635
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-10 11:42:23 +0100

    Better handling of expected wait times

--- frontend/components/HelpdeskItemOccupation/HelpdeskItemOccupation.js
+++ frontend/components/HelpdeskItemOccupation/HelpdeskItemOccupation.js
@@ -2,6 +2,7 @@
 
 import { useTranslations } from 'next-intl';
 import styles from './HelpdeskItemOccupation.module.css';
+import { useEffect, useState } from 'react';
 
 export default function HelpdeskItemOccupation({ currentlyWaiting, expectedWaitTime }) {
   //
@@ -10,6 +11,32 @@ export default function HelpdeskItemOccupation({ currentlyWaiting, expectedWaitT
   // A. Setup variables
 
   const t = useTranslations('HelpdeskItemOccupation');
+
+  const [waitTimeString, setWaitTimeString] = useState('');
+
+  //
+  // C. Render components
+
+  useEffect(() => {
+    // When there are customers waiting
+    if (currentlyWaiting > 0) {
+      if (expectedWaitTime < 60) {
+        // If wait time is less than a minute, then round it to the minute
+        setWaitTimeString('< 1min');
+      } else if (expectedWaitTime < 3600) {
+        // If wait time is less than an hour
+        const minutes = Math.floor(expectedWaitTime / 60);
+        setWaitTimeString(`${minutes} min`);
+      } else {
+        // If wait time is more than an hour
+        const hours = Math.floor(expectedWaitTime / 3600);
+        const minutes = Math.floor((expectedWaitTime % 3600) / 60);
+        setWaitTimeString(`${hours} h ${minutes} min`);
+      }
+    } else {
+      setWaitTimeString(`Immediate`);
+    }
+  }, [currentlyWaiting, expectedWaitTime]);
 
   //
   // C. Render components
@@ -20,7 +47,7 @@ export default function HelpdeskItemOccupation({ currentlyWaiting, expectedWaitT
         <p className={styles.text}>{t('currently_waiting.label', { value: currentlyWaiting })}</p>
       </div>
       <div className={styles.container}>
-        <p className={styles.text}>{t('expected_wait_time.label', { value: expectedWaitTime })}</p>
+        <p className={styles.text}>{t('expected_wait_time.label', { value: waitTimeString })}</p>
       </div>
     </>
   );
---
+++ frontend/services/formatSecondsToTime.js
@@ -0,0 +1,24 @@
+/* * */
+/* formatSecondsToTime */
+/* Explanation needed. */
+/* * */
+
+export default function formatSecondsToTime(timeInSeconds, options) {
+  //
+  if (!timeInSeconds && timeInSeconds !== 0) return '•••';
+
+  if (timeInSeconds < 60) {
+    return timeInSeconds + ' seg';
+  } else if (timeInSeconds < 3600) {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    if (seconds > 0) return `${minutes} min ${seconds} seg`;
+    else return `${minutes} min`;
+  } else {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+    return `${hours} h ${minutes} min ${seconds} seg`;
+  }
+  //
+}
