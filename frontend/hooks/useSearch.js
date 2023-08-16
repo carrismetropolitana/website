4c65348d5b9ad6b7467423849a60c619faf0bba8
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-17 00:17:54 +0100

    Regex option on query

--- frontend/hooks/useSearch.js
+++ frontend/hooks/useSearch.js
@@ -6,8 +6,8 @@ export default function useSearch(query, data, options) {
   // Return all data if query is empty
   if (!query) return data;
 
-  // Normalize the query to remove non-English characters
-  const normalizedQuery = transliterate(query.toLowerCase());
+  // Normalize the query
+  const normalizedQuery = normalizeString(query, { toLowerCase: true, transliterate: true, regexReplace: options.regexReplace });
 
   // Check if 'keys' option is present
   if (options && options.keys) {
@@ -16,8 +16,8 @@ export default function useSearch(query, data, options) {
       for (let key of options.keys) {
         if (item.hasOwnProperty(key)) {
           const value = item[key];
-          const stringifiedValue = value ? String(value).toLowerCase() : '';
-          const normalizedValue = transliterate(stringifiedValue);
+          const stringifiedValue = value ? String(value) : '';
+          const normalizedValue = normalizeString(stringifiedValue, { toLowerCase: true, transliterate: true, regexReplace: options.regexReplace });
           if (normalizedValue.includes(normalizedQuery)) {
             hasMatch = true;
             break;
@@ -30,10 +30,24 @@ export default function useSearch(query, data, options) {
 
   // The case where options is not defined
   return data.filter((item) => {
-    const stringifiedData = Object.values(item).join('').toLowerCase();
-    const normalizedItemValue = transliterate(stringifiedData);
-    return normalizedItemValue.includes(normalizedQuery);
+    const stringifiedValue = Object.values(item).join('');
+    const normalizedValue = normalizeString(stringifiedValue, { toLowerCase: true, transliterate: true, regexReplace: options.regexReplace });
+    return normalizedValue.includes(normalizedQuery);
   });
 
+  //
+}
+
+function normalizeString(rawString, options) {
+  // Initiate a variable to hold several normalization steps
+  let normalizedString = rawString;
+  // Convert the string to lowecase characters
+  if (options && options.toLowerCase) normalizedString = normalizedString.toLowerCase();
+  // Use inflected to remove non-English characters
+  if (options && options.transliterate) normalizedString = transliterate(normalizedString);
+  // Check if 'regex' option is present and replace forbidden characters
+  if (options && options.regexReplace) normalizedString = normalizedString.replace(options.regexReplace, '');
+  // Return result
+  return normalizedString;
   //
 }
