2fe21cf53b9d334687dd866e0a727d660b5898bb
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-09-12 00:39:26 +0100

    CSS Cleanup

--- frontend/components/OSMMap/OSMMap.js
+++ frontend/components/OSMMap/OSMMap.js
@@ -15,7 +15,6 @@ export default function OSMMap({
   onMoveStart = () => {},
   onMoveEnd = () => {},
   interactiveLayerIds = [],
-  toolbar,
   scrollZoom = true,
   navigation = true,
   fullscreen = true,
