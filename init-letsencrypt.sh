2fd89592f04a4a915f1c8295478f33b66c7637d2
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-09-12 19:54:28 +0100

    Added another domain to server

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
-domain=beta.carrismetropolitana.pt
+domain1=beta.carrismetropolitana.pt
+domain2=on.carrismetropolitana.pt
 email="carrismetropolitana@gmail.com" # Adding a valid address is strongly recommended
 
 
@@ -14,18 +15,30 @@ curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/c
 curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "./letsencrypt/ssl-dhparams.pem"
 echo
 
-echo "### Creating dummy certificate for $domain and www.$domain ..."
-mkdir -p "./letsencrypt/live/$domain"
-mkdir -p "./letsencrypt/live/www.$domain"
+echo "### Creating dummy certificate for $domain1 and www.$domain1 and $domain2 and www.$domain2 ..."
+mkdir -p "./letsencrypt/live/$domain1"
+mkdir -p "./letsencrypt/live/www.$domain1"
+mkdir -p "./letsencrypt/live/$domain2"
+mkdir -p "./letsencrypt/live/www.$domain2"
 docker compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:4096 -days 1\
-    -keyout '/etc/letsencrypt/live/$domain/privkey.pem' \
-    -out '/etc/letsencrypt/live/$domain/fullchain.pem' \
+    -keyout '/etc/letsencrypt/live/$domain1/privkey.pem' \
+    -out '/etc/letsencrypt/live/$domain1/fullchain.pem' \
     -subj '/CN=localhost'" certbot
 docker compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:4096 -days 1\
-    -keyout '/etc/letsencrypt/live/www.$domain/privkey.pem' \
-    -out '/etc/letsencrypt/live/www.$domain/fullchain.pem' \
+    -keyout '/etc/letsencrypt/live/www.$domain1/privkey.pem' \
+    -out '/etc/letsencrypt/live/www.$domain1/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+docker compose run --rm --entrypoint "\
+  openssl req -x509 -nodes -newkey rsa:4096 -days 1\
+    -keyout '/etc/letsencrypt/live/$domain2/privkey.pem' \
+    -out '/etc/letsencrypt/live/$domain2/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+docker compose run --rm --entrypoint "\
+  openssl req -x509 -nodes -newkey rsa:4096 -days 1\
+    -keyout '/etc/letsencrypt/live/www.$domain2/privkey.pem' \
+    -out '/etc/letsencrypt/live/www.$domain2/fullchain.pem' \
     -subj '/CN=localhost'" certbot
 echo
 
@@ -34,18 +47,24 @@ echo "### Starting nginx ..."
 docker compose up --force-recreate -d nginx
 echo
 
-echo "### Deleting dummy certificate for $domain ..."
+echo "### Deleting dummy certificate for $domain1 and $domain2 ..."
 docker compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domain && \
-  rm -Rf /etc/letsencrypt/archive/$domain && \
-  rm -Rf /etc/letsencrypt/renewal/$domain.conf \
-  rm -Rf /etc/letsencrypt/live/www.$domain && \
-  rm -Rf /etc/letsencrypt/archive/www.$domain && \
-  rm -Rf /etc/letsencrypt/renewal/www.$domain.conf" certbot
+  rm -Rf /etc/letsencrypt/live/$domain1 && \
+  rm -Rf /etc/letsencrypt/archive/$domain1 && \
+  rm -Rf /etc/letsencrypt/renewal/$domain1.conf \
+  rm -Rf /etc/letsencrypt/live/www.$domain1 && \
+  rm -Rf /etc/letsencrypt/archive/www.$domain1 && \
+  rm -Rf /etc/letsencrypt/renewal/www.$domain1.conf \
+  rm -Rf /etc/letsencrypt/live/$domain2 && \
+  rm -Rf /etc/letsencrypt/archive/$domain2 && \
+  rm -Rf /etc/letsencrypt/renewal/$domain2.conf \
+  rm -Rf /etc/letsencrypt/live/www.$domain2 && \
+  rm -Rf /etc/letsencrypt/archive/www.$domain2 && \
+  rm -Rf /etc/letsencrypt/renewal/www.$domain2.conf" certbot
 echo
 
 
-echo "### Requesting Let's Encrypt certificate for $domain and www.$domain ..."
+echo "### Requesting Let's Encrypt certificate for $domain1 and www.$domain1 and $domain2 and www.$domain2 ..."
 
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
@@ -53,7 +72,7 @@ if [ $staging != "0" ]; then staging_arg="--staging"; fi
 docker compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
-    -d $domain -d www.$domain \
+    -d $domain1 -d www.$domain1 -d $domain2 -d www.$domain2 \
     --email $email \
     --rsa-key-size 4096 \
     --agree-tos \
