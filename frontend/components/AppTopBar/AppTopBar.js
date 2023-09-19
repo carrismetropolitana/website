8fe3792a779766002fbf90342c9589f04c124ee5
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-09-19 19:03:17 +0100

    Cleanup

--- frontend/components/AppTopBar/AppTopBar.js
+++ frontend/components/AppTopBar/AppTopBar.js
@@ -1,17 +1,6 @@
 import styles from './AppTopBar.module.css';
 
 export default function AppTopBar() {
-  //
-
-  const AppTopBarLink = ({ href = '#', label, active }) => (
-    <a href={href} className={`${styles.link} ${active && styles.active}`}>
-      <div className={styles.çabel}>{label}</div>
-      <div className={styles.indicatorWrapper}>
-        <div className={styles.indicatorActive} />
-      </div>
-    </a>
-  );
-
   return (
     <div className={styles.container}>
       <a href="//www.tmlmobilidade.pt/" className={styles.link}>
