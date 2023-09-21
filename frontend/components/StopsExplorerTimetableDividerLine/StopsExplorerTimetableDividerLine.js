4d098518a92626e18900c4688c3c18f8cde2a7b8
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-09-21 23:28:35 +0100

    Added current time

--- frontend/components/StopsExplorerTimetableDividerLine/StopsExplorerTimetableDividerLine.js
+++ frontend/components/StopsExplorerTimetableDividerLine/StopsExplorerTimetableDividerLine.js
@@ -6,12 +6,24 @@ export default function StopsExplorerTimetableDividerLine() {
   //
 
   //
-  // A. Render components
+  // A. Setup variables
+
+  const currentTime = new Date();
+  const currentHours = currentTime.getHours();
+  const currentMinutes = currentTime.getMinutes();
+
+  //
+  // B. Render components
 
   return (
     <div className={styles.container}>
       <div className={styles.circle}></div>
       <div className={styles.line}></div>
+      <div className={styles.time}>
+        {currentHours}
+        <span>:</span>
+        {currentMinutes}
+      </div>
     </div>
   );
 }
