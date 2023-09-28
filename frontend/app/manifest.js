544f9087bb3b6ad34fcb7e72a553f3b63216d38f
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-09-28 01:53:40 +0100

    Updated icons

--- frontend/app/manifest.js
+++ frontend/app/manifest.js
@@ -4,8 +4,8 @@ export default function manifest() {
     short_name: 'Horários',
     description: 'Horários em Tempo Real da Carris Metropolitana',
     start_url: '/',
-    display: 'fullscreen',
-    background_color: '#FFFFFF',
-    theme_color: '#FFDD00',
+    theme_color: '#ffffff',
+    background_color: '#ffffff',
+    display: 'standalone',
   };
 }
