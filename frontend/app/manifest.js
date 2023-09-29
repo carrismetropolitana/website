1ff1b2fc68cb859123aa376f64f70b9f21fbc8b2
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-09-29 11:51:43 +0100

    Adjusted icons

--- frontend/app/manifest.js
+++ frontend/app/manifest.js
@@ -7,5 +7,22 @@ export default function manifest() {
     theme_color: '#000000',
     background_color: '#000000',
     display: 'standalone',
+    icons: [
+      {
+        src: '/icon.svg',
+        type: 'image/svg+xml',
+        sizes: '512x512',
+      },
+      {
+        src: 'icon1.png',
+        type: 'image/png',
+        sizes: '192x192',
+      },
+      {
+        src: 'icon2.png',
+        type: 'image/png',
+        sizes: '512x512',
+      },
+    ],
   };
 }
