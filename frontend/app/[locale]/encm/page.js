f45819f5a24e1d9b71f1cc945fadfd658433c03d
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-10-10 23:42:24 +0100

    Adjusted meta title

--- frontend/app/[locale]/encm/page.js
+++ frontend/app/[locale]/encm/page.js
@@ -1,6 +1,20 @@
 import { OneFullColumn } from '@/components/Layouts/Layouts';
 import EncmExplorer from '@/components/EncmExplorer/EncmExplorer';
 
+/* * */
+
+export async function generateMetadata({ params }) {
+  switch (params.locale) {
+    case 'pt':
+      return { title: 'Espaços navegante® Carris Metropolitana', description: 'Tempos de atendimento estimados para todos os Espaços navegante® Carris Metropolitana.' };
+    default:
+    case 'en':
+      return { title: 'Espaços navegante® Carris Metropolitana', description: 'Tempos de atendimento estimados para todos os Espaços navegante® Carris Metropolitana.' };
+  }
+}
+
+/* * */
+
 export default function Page() {
   //
 
