781fdf66e81cec8d466b82fb4c67879586b64316
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-12-27 04:06:53 +0000

    Cleanup

--- nextjs/schemas/SignIn/default.js
+++
@@ -1,6 +0,0 @@
-/* * */
-
-export const SignInDefault = {
-  email: '',
-  password: '',
-};
--- nextjs/schemas/SignIn/validation.js
+++ nextjs/schemas/SignIn/validation.js
@@ -5,5 +5,5 @@ import * as yup from 'yup';
 /* * */
 
 export const SignInValidation = yup.object({
-  email: yup.string().email('Please provide a valid email address.').required('Please enter your TML email.'),
+  email: yup.string().email('Please provide a valid email address.').required('Please enter your email.'),
 });
