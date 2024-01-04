be64a74649620b0f6176ae794d77f2bd72af0d9d
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-01-04 00:48:44 +0000

    Cleanup

--- nextjs/components/FrontendStopsStopInfo/FrontendStopsStopInfo.js
+++ nextjs/components/FrontendStopsStopInfo/FrontendStopsStopInfo.js
@@ -1,5 +1,7 @@
 'use client';
 
+/* * */
+
 import styles from './FrontendStopsStopInfo.module.css';
 import CopyBadge from '@/components/CopyBadge/CopyBadge';
 import FacilityIcon from '@/components/Facilities/FacilityIcon';
@@ -16,39 +18,39 @@ export default function FrontendStopsStopInfo() {
   //
   // A. Setup variables
 
-  const FrontendStopsContext = useFrontendStopsContext();
-  const stopLocation = useStopLocation(FrontendStopsContext.entities.stop);
+  const frontendStopsContext = useFrontendStopsContext();
+  const stopLocation = useStopLocation(frontendStopsContext.entities.stop);
 
   //
   // B. Render components
 
   return (
-    FrontendStopsContext.entities.stop && (
+    frontendStopsContext.entities.stop && (
       <div className={styles.container}>
         <div className={styles.badges}>
-          <CopyBadge label={`#${FrontendStopsContext.entities.stop.id}`} value={FrontendStopsContext.entities.stop.id} />
-          <CopyBadge label={`${FrontendStopsContext.entities.stop.lat}, ${FrontendStopsContext.entities.stop.lon}`} value={`${FrontendStopsContext.entities.stop.lat}	${FrontendStopsContext.entities.stop.lon}`} />
+          <CopyBadge label={`#${frontendStopsContext.entities.stop.id}`} value={frontendStopsContext.entities.stop.id} />
+          <CopyBadge label={`${frontendStopsContext.entities.stop.lat}, ${frontendStopsContext.entities.stop.lon}`} value={`${frontendStopsContext.entities.stop.lat}	${frontendStopsContext.entities.stop.lon}`} />
         </div>
 
-        <div className={styles.nameAndLocation} aria-label={FrontendStopsContext.entities.stop.tts_name || FrontendStopsContext.entities.stop.name}>
-          <h3 className={styles.stopName} aria-label={FrontendStopsContext.entities.stop.tts_name || FrontendStopsContext.entities.stop.name}>
-            <AudioBadge type="stops" id={FrontendStopsContext.entities.stop.id} />
-            {FrontendStopsContext.entities.stop.name}
+        <div className={styles.nameAndLocation} aria-label={frontendStopsContext.entities.stop.tts_name || frontendStopsContext.entities.stop.name}>
+          <h3 className={styles.stopName} aria-label={frontendStopsContext.entities.stop.tts_name || frontendStopsContext.entities.stop.name}>
+            <AudioBadge type="stops" id={frontendStopsContext.entities.stop.id} />
+            {frontendStopsContext.entities.stop.name}
           </h3>
           {stopLocation && <h5 className={styles.stopLocation}>{stopLocation}</h5>}
         </div>
 
-        {FrontendStopsContext.entities.stop.facilities.length > 0 && (
+        {frontendStopsContext.entities.stop.facilities.length > 0 && (
           <div className={styles.facilities}>
-            {FrontendStopsContext.entities.stop.facilities.map((e, index) => (
+            {frontendStopsContext.entities.stop.facilities.map((e, index) => (
               <FacilityIcon key={index} name={e} size={28} />
             ))}
           </div>
         )}
 
-        {FrontendStopsContext.entities.stop?.lines?.length > 0 && (
+        {frontendStopsContext.entities.stop?.lines?.length > 0 && (
           <div className={styles.lines}>
-            {FrontendStopsContext.entities.stop.lines.map((lineId, index) => (
+            {frontendStopsContext.entities.stop.lines.map((lineId, index) => (
               <NewLineBadge key={index} id={lineId} />
             ))}
           </div>
@@ -56,4 +58,6 @@ export default function FrontendStopsStopInfo() {
       </div>
     )
   );
+
+  //
 }
