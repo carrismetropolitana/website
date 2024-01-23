1dba2504f95949afa31f81688cb887060400cfa7
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-01-23 17:46:18 +0000

    Add StatusMessage

--- nextjs/app/[locale]/(frontend)/layout.js
+++ nextjs/app/[locale]/(frontend)/layout.js
@@ -4,17 +4,17 @@ import FrontendWrapper from '@/components/FrontendWrapper/FrontendWrapper';
 import FrontendBrandSwitcher from '@/components/FrontendBrandSwitcher/FrontendBrandSwitcher';
 import FrontendHeader from '@/components/FrontendHeader/FrontendHeader';
 import FrontendFooter from '@/components/FrontendFooter/FrontendFooter';
-// import StatusMessage from '@/components/StatusMessage/StatusMessage';
+import StatusMessage from '@/components/StatusMessage/StatusMessage';
 // import MaintenanceWarning from '@/components/MaintenanceWarning/MaintenanceWarning';
 
 /* * */
 
-export default function Layout({ children, params: { locale } }) {
+export default function Layout({ children }) {
   return (
     <FrontendWrapper>
       <FrontendBrandSwitcher />
       <FrontendHeader />
-      {/* <StatusMessage /> */}
+      <StatusMessage />
       {/* <MaintenanceWarning /> */}
       {children}
       <FrontendFooter />
