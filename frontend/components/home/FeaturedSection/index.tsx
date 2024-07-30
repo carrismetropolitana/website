fa24efe234d5c0d62eca018bf4c54526ae87724e
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-07-30 10:07:47 +0100

    Tweaks

--- frontend/components/home/FeaturedSection/index.tsx
+++ frontend/components/home/FeaturedSection/index.tsx
@@ -3,39 +3,12 @@
 import HomeFeaturedSectionCard from '@/components/home/FeaturedSectionCard';
 import HomeFeaturedSectionMetricDemand from '@/components/home/FeaturedSectionMetricDemand';
 import LayoutSection from '@/components/layout/Section';
-import { IconHelpHexagon, IconMessage, IconPhoneCheck, IconUmbrella, IconUserPentagon } from '@tabler/icons-react';
-import { useTranslations } from 'next-intl';
 
 import styles from './styles.module.css';
 
 /* * */
 
-const MENU_ITEMS = [
-	{ _id: 'faq', href: '/faq', icon: <IconHelpHexagon /> },
-	{ _id: 'lost-and-found', href: '/lost-and-found', icon: <IconUmbrella /> },
-	{ _id: 'complaints', href: '/complaints', icon: <IconMessage /> },
-	{ _id: 'stores', href: '/stores', icon: <IconUserPentagon /> },
-	{ _id: 'contacts', href: '/contacts', icon: <IconPhoneCheck /> },
-];
-
-/* * */
-
 export default function Component() {
-	//
-
-	//
-	// A. Setup variables
-
-	const t = useTranslations('HomeSupportSection');
-
-	//
-	// B. Transform data
-
-	const menuItemsFormatted = MENU_ITEMS.map(item => ({ ...item, label: t(`nav_items.${item._id}`) }));
-
-	//
-	// C. Render Components
-
 	return (
 		<LayoutSection withGlobalPadding>
 			<div className={styles.innerWrapper}>
@@ -45,6 +18,4 @@ export default function Component() {
 			</div>
 		</LayoutSection>
 	);
-
-	//
 }
