69a5aea207811b6f9e26090246638b3c031471d2
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-08-02 01:58:03 +0100

    Update index.tsx

--- frontend/components/home/FeaturedSection/index.tsx
+++ frontend/components/home/FeaturedSection/index.tsx
@@ -11,8 +11,9 @@ export default function Component() {
 	return (
 		<Section withChildrenPadding>
 			<div className={styles.innerWrapper}>
-				<FeaturedSectionCard coverImageSrc="/images/drivers.png" href="#" title="iudshuisd" />
-				<FeaturedSectionCard coverImageSrc="/images/loures.png" href="#" title="iudshuisd" />
+				<FeaturedSectionCard coverImageSrc="/images/drivers.png" href="#" title="1" />
+				<FeaturedSectionCard coverImageSrc="/images/loures.png" href="#" title="2" />
+				<FeaturedSectionCard coverImageSrc="#" href="#" title="3" />
 			</div>
 		</Section>
 	);
