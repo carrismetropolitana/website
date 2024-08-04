b04cffcea5086c15b0aafe487dc643ba59881fa5
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-08-04 17:00:09 +0100

    Update index.tsx

--- frontend/components/alerts/AlertsListItemImageThumbnail/index.tsx
+++ frontend/components/alerts/AlertsListItemImageThumbnail/index.tsx
@@ -1,5 +1,6 @@
 /* * */
 
+import { Link } from '@/translations/navigation';
 import { Image } from '@mantine/core';
 import { IconCircleArrowUpRightFilled } from '@tabler/icons-react';
 
@@ -17,9 +18,9 @@ interface AlertsListItemImageThumbnailProps {
 
 export default function Component({ alt, href, src }: AlertsListItemImageThumbnailProps) {
 	return (
-		<div className={styles.container}>
+		<Link className={styles.container} href={href}>
 			<IconCircleArrowUpRightFilled className={styles.icon} size={25} />
 			<Image alt={alt} className={styles.image} fallbackSrc="/planner/placeholder.png" src={src} />
-		</div>
+		</Link>
 	);
 }
