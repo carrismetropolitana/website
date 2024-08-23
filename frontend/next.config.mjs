3208f35e9094b2b735bfa852a9772c5ceb05bc2c
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-08-23 21:09:04 +0100

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -36,10 +36,10 @@ export default withNextIntl({
     ]
   },
   //   swcMinify: false,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization.minimize = false
-    }
-    return config
-  },
+  //   webpack: (config, { isServer }) => {
+  //     if (!isServer) {
+  //       config.optimization.minimize = false
+  //     }
+  //     return config
+  //   },
 })
