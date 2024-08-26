89b8ecaa003312ae05a554b8ba78e1ed0d176e75
ttmx <git@tteles.dev>
2024-08-26 16:41:23 +0000

    feat: Update composeTimetable function to include stop sequence and date

--- frontend/utils/composeTimetable.ts
+++ frontend/utils/composeTimetable.ts
@@ -8,7 +8,10 @@ import { TimetableDayStop } from '@/types/timetables.types';
  * @param mainPatternId Pattern id for which we should not add exceptions
  *
  */
-export default function composeTimetable(patternGroups: PatternGroup[], stopId: string, mainPatternId: string): TimetableDayStop {
+export default function composeTimetable(patternGroups: PatternGroup[], stopId: string, stopSequence: number, mainPatternId: string, date: Date): TimetableDayStop {
+	// Format date
+	const formattedDate = `${date.getFullYear()}${(date.getMonth() + 1).toString().padStart(2, '0')}${date.getDate().toString().padStart(2, '0')}`;
+
 	// Initialize an empty object to store the final timetable
 	const timetableDayStop: TimetableDayStop = {
 		exceptions: [],
@@ -34,8 +37,11 @@ export default function composeTimetable(patternGroups: PatternGroup[], stopId: 
 	// Loop through each pattern group in the pattern data
 	patternGroups.forEach((patternGroup) => {
 		patternGroup.trips.forEach((trip) => {
+			if (!trip.dates.includes(formattedDate)) {
+				return;
+			}
 			trip.schedule.forEach((schedule) => {
-				if (schedule.stop_id !== stopId) {
+				if (schedule.stop_id !== stopId && schedule.stop_sequence !== stopSequence) {
 					return;
 				}
 				// Parse arrival time
