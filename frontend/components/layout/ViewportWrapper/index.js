c7ca1cc5213833acea873b9e6872203755303620
ttmx <git@tteles.dev>
2024-08-28 09:47:46 +0000

    Color experiments

--- frontend/components/layout/ViewportWrapper/index.js
+++ frontend/components/layout/ViewportWrapper/index.js
@@ -5,6 +5,8 @@
 import AnalyticsConsentPopup from '@/components/analytics/ConsentPopup';
 import Footer from '@/components/footer/Footer';
 import Header from '@/components/header/Header';
+import { useDebugContext } from '@/contexts/Debug.context';
+import { ColorPicker } from '@mantine/core';
 import { useSearchParams } from 'next/navigation';
 
 import styles from './styles.module.css';
@@ -18,6 +20,7 @@ export default function Component({ children }) {
 	if (originParam && (originParam === 'app' || originParam === 'mupi')) {
 		hasHeader = false;
 	}
+	const debug = useDebugContext();
 
 	return (
 		<div className={styles.container}>
@@ -25,6 +28,20 @@ export default function Component({ children }) {
 			{children}
 			{hasHeader && <Footer />}
 			{hasHeader && <AnalyticsConsentPopup />}
+			{debug.flags.is_debug_mode && (
+				<div style={{ bottom: 0, position: 'fixed', right: 0 }}>
+
+					<ColorPicker
+						defaultValue={{ h: 240, l: 0.08, s: 0.12 }}
+						format="hsl"
+						onChange={
+							(color) => {
+								document.documentElement.style.setProperty('--hsl', color);
+							}
+						}
+					/>
+				</div>
+			)}
 		</div>
 	);
 }
