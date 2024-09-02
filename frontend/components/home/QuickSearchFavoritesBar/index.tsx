46d79cff22c8dcadcfee50ca8eb1eebc82a876be
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-09-02 15:59:58 +0100

    Update QuickSearchFavoritesBar to fetch and display favorite lines

--- frontend/components/home/QuickSearchFavoritesBar/index.tsx
+++ frontend/components/home/QuickSearchFavoritesBar/index.tsx
@@ -2,8 +2,12 @@
 
 /* * */
 
+import Loader from '@/components/common/Loader';
 import LineBadge from '@/components/lines/LineBadge';
+import { useProfileContext } from '@/contexts/Profile.context';
 import { useTranslations } from 'next-intl';
+import { useMemo } from 'react';
+import useSWR from 'swr';
 
 import styles from './styles.module.css';
 
@@ -16,21 +20,49 @@ export default function Component() {
 	// A. Setup variables
 
 	const t = useTranslations('HomeQuickSearchFavoritesBar');
+	const profileContext = useProfileContext();
 
 	//
-	// B. Render Components
+	// B. Fetch data
 
-	// return (
-	// 	<div className={styles.container}>
-	// 		<p className={styles.emptyMessage}>{t('empty')}</p>
-	// 	</div>
-	// );
+	const { data: allLinesData, isLoading: allLinesLoading } = useSWR('https://api.carrismetropolitana.pt/v2/lines');
+
+	//
+	// C. Transform data
+
+	const favoriteLinesData = useMemo(() => {
+		if (!allLinesData || !profileContext.data.profile?.favorite_lines?.length) {
+			return [];
+		};
+		const filteredFavoriteLines = allLinesData.filter(line => profileContext.data.profile?.favorite_lines?.includes(line.line_id));
+		const sortedFavoriteLines = filteredFavoriteLines.sort((a, b) => a.short_name.localeCompare(b.short_name));
+		return sortedFavoriteLines;
+	}, [allLinesData, profileContext.data.profile?.favorite_lines]);
+
+	//
+	// D. Render Components
+
+	if (allLinesLoading || profileContext.flags.is_loading) {
+		return (
+			<div className={styles.container}>
+				<Loader size={26} visible />
+			</div>
+		);
+	}
+
+	if (!allLinesData || !profileContext.data.profile || profileContext.data.profile.favorite_lines?.length === 0) {
+		return (
+			<div className={styles.container}>
+				<p className={styles.emptyMessage}>{t('empty')}</p>
+			</div>
+		);
+	}
 
 	return (
 		<div className={styles.container}>
-			<LineBadge onClick={() => { alert('here'); }} shortName="1234" withAlertIcon />
-			<LineBadge onClick={() => { alert('here'); }} shortName="1234" withAlertIcon />
-			<LineBadge onClick={() => { alert('here'); }} shortName="1234" withAlertIcon />
+			{favoriteLinesData.map(favoriteLine => (
+				<LineBadge key={favoriteLine.line_id} line={favoriteLine} />
+			))}
 		</div>
 	);
 
