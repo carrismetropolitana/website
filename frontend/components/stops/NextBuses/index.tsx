f91684658705950c266b20dc8fcac04b475d28a3
ttmx <git@tteles.dev>
2024-09-09 16:10:31 +0000

    Add translation to NextBuses

--- frontend/components/stops/NextBuses/index.tsx
+++ frontend/components/stops/NextBuses/index.tsx
@@ -1,6 +1,7 @@
 import NoDataLabel from '@/components/layout/NoDataLabel';
 import { useStopsSingleContext } from '@/contexts/StopsSingle.context';
 import { StopRealtime } from '@/types/stops.types';
+import { useTranslations } from 'next-intl';
 import { useState } from 'react';
 
 import { NextBusRow } from '../NextBusRow';
@@ -17,6 +18,7 @@ export default function NextBuses() {
 	const stopsSingleContext = useStopsSingleContext();
 	const [showPrevious, setShowPrevious] = useState(false);
 	const realtimeData = stopsSingleContext.data.realtime;
+	const t = useTranslations('stops.NextBuses');
 
 	// B. Transform data
 	const now_seconds = Date.now() / 1000;
@@ -24,14 +26,14 @@ export default function NextBuses() {
 	const lastRealtime = previousRealtimeData?.[previousRealtimeData.length - 1];
 	const nextRealtimeData = realtimeData?.filter(realtime => bestTime(realtime) >= now_seconds);
 
+	// C. Render components
 	return (
 		<div className={styles.container}>
 			<NextBusesHeader />
 			<div className={styles.toggle} onClick={() => setShowPrevious(!showPrevious)}>
-				{showPrevious ? 'Ocultar passagens anteriores' : 'Mostrar passagens anteriores'}
+				{showPrevious ? t('hide') : t('show')}
 			</div>
 			<div>
-
 				{!showPrevious ? lastRealtime && <NextBusRow realtime={lastRealtime} />
 					: previousRealtimeData && previousRealtimeData.map(realtime => (
 						<NextBusRow key={realtime.pattern_id + '-' + realtime.scheduled_arrival} realtime={realtime} />
