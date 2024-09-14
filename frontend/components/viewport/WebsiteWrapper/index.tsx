992c92c61bc77f1dd867f9346af26598a9ed9fe3
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-09-14 10:18:04 +0100

    Restore origin check

--- frontend/components/viewport/WebsiteWrapper/index.tsx
+++ frontend/components/viewport/WebsiteWrapper/index.tsx
@@ -1,20 +1,35 @@
+'use client';
+
 /* * */
 
 import AnalyticsConsentPopup from '@/components/analytics/ConsentPopup';
 import Footer from '@/components/footer/Footer';
 import Header from '@/components/header/Header';
+import { useSearchParams } from 'next/navigation';
 
 import styles from './styles.module.css';
 
 /* * */
 
 export default function Component({ children }) {
+	//
+
+	//
+	// A. Setup variables
+
+	const searchParams = useSearchParams();
+
+	//
+	// B. Render components
+
 	return (
 		<div className={styles.container}>
-			<Header />
+			{searchParams.get('origin') !== 'app' && <Header />}
 			{children}
-			<Footer />
-			<AnalyticsConsentPopup />
+			{searchParams.get('origin') !== 'app' && <Footer />}
+			{searchParams.get('origin') !== 'app' && <AnalyticsConsentPopup />}
 		</div>
 	);
+
+	//
 }
