f32ef2283b425e89af6315bcf55b37022c313fa3
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-09-14 01:07:45 +0100

    Add / to url

--- frontend/app/api/app-ios/v1/startup/message/route.ts
+++ frontend/app/api/app-ios/v1/startup/message/route.ts
@@ -9,14 +9,14 @@ const appStartupMessages: StartupMessage[] = [
 		max_build: 1,
 		min_build: 2,
 		presentation_type: 'breaking',
-		url_host: 'https://alpha.carrismetropolitana.pt',
+		url_host: 'https://alpha.carrismetropolitana.pt/',
 		url_path: '/views/app/startup/messages/abc/?locale=pt&appVersion=1.2.3',
 	},
 	{
 		max_build: 1,
 		min_build: 2,
 		presentation_type: 'changelog',
-		url_host: 'https://alpha.carrismetropolitana.pt',
+		url_host: 'https://alpha.carrismetropolitana.pt/',
 		url_path: '/views/app/startup/messages/abc/?locale=pt&appVersion=1.2.3',
 	},
 ];
