78d56bb6828ad4e4919b7d78536b48c40e5ef66c
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-09-16 17:17:47 +0100

    Add SelectPattern component and useLinePatterns hook; update AppError and pt-PT translations

---
+++ frontend/hooks/useLinePattern.ts
@@ -0,0 +1,58 @@
+import { Pattern, PatternGroup } from '@/types/lines.types';
+import { useState } from 'react';
+
+export interface UseLinePatterns {
+	error: boolean
+	fetchPatterns: (pattern_ids: string[]) => void
+	loading: boolean
+	patterns: PatternGroup[] | null
+}
+
+const useLinePatterns = () => {
+	const [loading, setLoading] = useState<boolean>(false);
+	const [error, setError] = useState<boolean>(false);
+	const [patterns, setPatterns] = useState<PatternGroup[] | null>(null);
+
+	const fetchPatterns = async (pattern_ids: string[]) => {
+		setError(false);
+		setPatterns(null);
+
+		if (pattern_ids.length <= 0) return;
+
+		setLoading(true);
+
+		try {
+			const fetchPromises = pattern_ids.map(patternId =>
+				fetch(`https://api.carrismetropolitana.pt/v2/patterns/${patternId}`).then(response => response.json()),
+			);
+			const resultData: Pattern[] = await Promise.all(fetchPromises);
+
+			// Map the data to the correct format
+			const groups: PatternGroup[] = [];
+			resultData.map((pattern: Pattern) => {
+				pattern.map((group: PatternGroup) => {
+					console.log('Group:', group);
+					groups.push(group);
+				});
+			});
+
+			setPatterns(groups);
+		}
+		catch (error) {
+			console.error('Error fetching pattern data:', error);
+			setError(true);
+		}
+		finally {
+			setLoading(false);
+		}
+	};
+
+	return {
+		error,
+		fetchPatterns,
+		loading,
+		patterns,
+	} as UseLinePatterns;
+};
+
+export default useLinePatterns;
