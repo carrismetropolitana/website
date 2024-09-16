127e57850b2d86d210e0a1ff7ba26533f49119d8
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-09-16 11:53:41 +0100

    Update route.ts

--- frontend/app/api/app-android/v1/startup/message/route.ts
+++ frontend/app/api/app-android/v1/startup/message/route.ts
@@ -8,7 +8,7 @@ const appStartupMessages: StartupMessage[] = [
 	{
 		build_max: 1826481590,
 		build_min: null,
-		message_id: 'ANDROID-0001',
+		message_id: 'ANDROID-0002',
 		presentation_type: 'changelog',
 		url_host: 'https://alpha.carrismetropolitana.pt/',
 		url_path: '/app-android/startup/message-1',
@@ -16,7 +16,7 @@ const appStartupMessages: StartupMessage[] = [
 	{
 		build_max: 1,
 		build_min: 2,
-		message_id: 'ANDROID-0002',
+		message_id: 'ANDROID-0005',
 		presentation_type: 'changelog',
 		url_host: 'https://alpha.carrismetropolitana.pt/',
 		url_path: '/views/app/startup/messages/abc/?locale=pt&appVersion=1.2.3',
