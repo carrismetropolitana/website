f8b829e5b3291830af674118778f787f8762db3d
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-09-16 11:06:47 +0100

    Add android message

--- frontend/app/api/app-android/v1/startup/message/route.ts
+++ frontend/app/api/app-android/v1/startup/message/route.ts
@@ -6,12 +6,12 @@ import type { StartupMessage } from '@/types/app.types';
 
 const appStartupMessages: StartupMessage[] = [
 	{
-		build_max: 1,
-		build_min: 2,
+		build_max: 99999999999,
+		build_min: null,
 		message_id: 'ANDROID-0001',
-		presentation_type: 'breaking',
+		presentation_type: 'changelog',
 		url_host: 'https://alpha.carrismetropolitana.pt/',
-		url_path: '/views/app/startup/messages/abc/?locale=pt&appVersion=1.2.3',
+		url_path: '/app-ios/startup/message-1',
 	},
 	{
 		build_max: 1,
