6d47270ae794a9ab1b57d992b7f7069f003e3b36
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-09-18 10:33:50 +0100

    Refactor file extension from .js to .tsx and add type annotation for news_id parameter

--- frontend/app/[locale]/(website)/news/[news_id]/page.tsx
+++ frontend/app/[locale]/(website)/news/[news_id]/page.tsx
@@ -4,6 +4,6 @@ import SinglePage from 'components/news/SinglePage';
 
 /* * */
 
-export default function Page({ params: { news_id } }) {
+export default function Page({ params: { news_id } }: { params: { news_id: string } }) {
 	return <SinglePage newsId={news_id} />;
 }
