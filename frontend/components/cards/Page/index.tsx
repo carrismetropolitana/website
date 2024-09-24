6d01f3b0f39261685bf473827be63645249011a5
Josue <jusi.monteiro@protonmail.com>
2024-09-24 18:05:37 +0100

    Added Modalities

--- frontend/components/cards/Page/index.tsx
+++ frontend/components/cards/Page/index.tsx
@@ -1,11 +1,15 @@
 /* * */
 
+import Modalities from '../modalities';
 import Pass from '../Pass';
 
 /* * */
 
 export default function Component() {
 	return (
-		<Pass />
+		<>
+			<Pass />
+			<Modalities />
+		</>
 	);
 }
