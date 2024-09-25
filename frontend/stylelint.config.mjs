22550fdc3f3567064a96bacd2955d9823c511784
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-09-25 15:26:11 +0100

    Refactor stylelint.config.mjs to allow additional CSS units

--- frontend/stylelint.config.mjs
+++ frontend/stylelint.config.mjs
@@ -6,4 +6,9 @@ import { css } from '@carrismetropolitana/eslint'
 
 /* * */
 
-export default css
+export default {
+  ...css,
+  rules: {
+    'unit-allowed-list': ['em', 'rem', 's', 'px', 'vh', 'vw', 'deg', 'fr', 'ms', 's', 'turn', 'grad', 'rad', 'dpi', 'dppx', 'dpcm', '%'],
+  },
+}
