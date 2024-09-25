4f60d51200f6ea610aa0648030e1d6c72c061d26
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-09-25 17:50:14 +0100

    Refactor stylelint.config.mjs to enforce camelCase selector class pattern

--- frontend/stylelint.config.mjs
+++ frontend/stylelint.config.mjs
@@ -9,6 +9,12 @@ import { css } from '@carrismetropolitana/eslint'
 export default {
   ...css,
   rules: {
+    'selector-class-pattern': [
+      /^[a-z]+([A-Z][a-z0-9]*)*$/,
+      {
+        message: 'Selector should be written in camelCase.',
+      },
+    ],
     'unit-allowed-list': ['em', 'rem', 's', 'px', 'vh', 'vw', 'deg', 'fr', 'ms', 's', 'turn', 'grad', 'rad', 'dpi', 'dppx', 'dpcm', '%'],
   },
 }
