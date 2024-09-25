aeef2997683fd899602c646a5d0acb118eca198a
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-09-25 15:58:50 +0100

    Refactor Pass component to use Card component and update styles

--- frontend/components/cards/Pass/index.tsx
+++ frontend/components/cards/Pass/index.tsx
@@ -1,31 +1,40 @@
 /* * */
 
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/common/Card';
 import Section from '@/components/layout/Section';
 import { IconsCommon, ImagesCommon } from '@/utils/assets';
+import { Image } from '@mantine/core';
 
 import styles from './styles.module.css';
 
 /* * */
-
 export default function Component() {
 	return (
 		<Section heading="Passe Mensal" subheading="Descubra quanto custa viajar na CMetropolitana de vez em quando. Para utilizações mais frequentes sugerimos a adesão ao sistema navegante®." withChildrenPadding>
 			<div className={styles.content}>
-				<div className={styles.card}>
-					<div className={styles.images}>
-						<img alt="Passe Mensal" src={IconsCommon.COINS} />
-						<img alt="Passe Mensal" src={IconsCommon.RECEIPT} />
-					</div>
-					<h2>Descruba a melhor opção para si.</h2>
-					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo ratione architecto, veniam mollitia temporibus fugit expedita odit? Accusantium, velit corporis voluptatem debitis necessitatibus voluptate officia, sequi atque distinctio sed a.</p>
-				</div>
-				<div className={styles.card}>
-					<div className={styles.images}>
-						<img alt="Passe Mensal" src={ImagesCommon.NAVEGANTE_CARD} />
-					</div>
-					<h2>Já tem um cartão personalizado?</h2>
-					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, fugit delectus atque ex, necessitatibus ullam suscipit odit cupiditate quis accusantium corrupti numquam, voluptatibus soluta? Sit dignissimos blanditiis autem tenetur minus.</p>
-				</div>
+				<Card>
+					<CardHeader>
+						<div className={styles.images}>
+							<Image alt="Icon Coins" src={IconsCommon.QUESTION} />
+							<Image alt="Icon Receipt" src={IconsCommon.TICKET} />
+						</div>
+					</CardHeader>
+					<CardContent>
+						<CardTitle>Descruba a melhor opção para si.</CardTitle>
+						<CardDescription>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ea natus itaque qui iure nam consequuntur minima, quibusdam saepe odio molestiae soluta aspernatur, corrupti labore fugiat? Numquam blanditiis accusantium quibusdam!</CardDescription>
+					</CardContent>
+				</Card>
+				<Card>
+					<CardHeader>
+						<div className={styles.images}>
+							<Image alt="Bilhete Navegante Ocasional" src={ImagesCommon.NAVEGANTE_CARD} />
+						</div>
+					</CardHeader>
+					<CardContent>
+						<CardTitle>Já tem um cartão personalizado?</CardTitle>
+						<CardDescription>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda magnam incidunt nam pariatur, molestias sequi beatae modi odio cumque a nesciunt facilis ad. Provident, ullam. Quae vero quis dolor animi.</CardDescription>
+					</CardContent>
+				</Card>
 			</div>
 		</Section>
 	);
