447a08312d41d413f210085b759d252d6acf69af
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-09-26 12:30:00 +0100

    Disable

--- frontend/utils/map.ts
+++ frontend/utils/map.ts
@@ -5,15 +5,32 @@ import * as turf from '@turf/turf';
 
 /* * */
 
-export const moveMap = (mapObject, coordinates) => {
+/**
+ *
+ * @param mapObject THe map that should be manipulated
+ * @param coordinates The destination coordinates to move the map to
+ * @param options Optional settings to customize the movement
+ */
+
+export const moveMap = (mapObject, coordinates, options) => {
+	//
+
+	//
 	// Get map current zoom level
+
 	const currentZoom = mapObject.getZoom();
 	const currentZoomWithMargin = currentZoom + MAP_DEFAULT_OPTIONS.zoomMargin;
+
+	//
 	// Check if the given coordinates are inside the currently rendered map bounds
+
 	const currentMapBounds: [[number, number], [number, number]] = mapObject.getBounds().toArray();
 	console.log(currentMapBounds);
 	const isInside = turf.booleanIntersects(turf.point(coordinates), turf.bboxPolygon([...currentMapBounds[0], ...currentMapBounds[1]]));
+
+	//
 	// If the given coordinates are visible and the zoom is not too far back (plus a little margin)...
+
 	if (isInside && currentZoomWithMargin > MAP_DEFAULT_OPTIONS.zoom) {
 		// ...then simply ease to it.
 		mapObject.easeTo({ center: coordinates, duration: MAP_DEFAULT_OPTIONS.speed * 0.25, zoom: currentZoom });
@@ -22,4 +39,6 @@ export const moveMap = (mapObject, coordinates) => {
 		// If the zoom is too far, or the given coordinates are not visible, then fly to it
 		mapObject.flyTo({ center: coordinates, duration: MAP_DEFAULT_OPTIONS.speed, zoom: MAP_DEFAULT_OPTIONS.zoom });
 	}
+
+	//
 };
