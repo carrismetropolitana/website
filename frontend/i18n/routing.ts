0027bffd31a85ddf33ceca40fe7e2d8f739264c4
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-10-01 13:32:13 +0100

    Refactor routing to add default translated pathnames for available locales

--- frontend/i18n/routing.ts
+++ frontend/i18n/routing.ts
@@ -1,6 +1,6 @@
 /* * */
 
-import { defaultLocaleCode, enabledLocaleCodes } from '@/i18n/config';
+import { availableLocales, defaultLocaleCode, enabledLocaleCodes } from '@/i18n/config';
 import { Routes } from '@/utils/routes';
 import { createLocalizedPathnamesNavigation } from 'next-intl/navigation';
 import { defineRouting } from 'next-intl/routing';
@@ -9,7 +9,16 @@ import { defineRouting } from 'next-intl/routing';
 const intlPathnames = Object.entries(Routes).reduce(
 	(acc: Record<string, Record<string, string>>, [, value]) => {
 		if (typeof value === 'object' && 'route' in value && 'intl' in value) {
-			acc[value['route']] = value['intl'];
+			const translatedPathnames = value['intl'] as Record<string, string>;
+			acc[value['route']] = translatedPathnames;
+
+			// Check if any of the available locales is not in translatedPathnames
+			// If not, add the default locale
+			for (const locale of availableLocales) {
+				if (locale.enabled && !(locale.value in translatedPathnames)) {
+					acc[value['route']][locale.value] = value['route'];
+				}
+			}
 		}
 		return acc;
 	},
