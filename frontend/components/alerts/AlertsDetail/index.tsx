ab8728f2f7050c1871bae468999536a6f6601785
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-10-07 17:17:39 +0100

    Cleanup more components

--- frontend/components/alerts/AlertsDetail/index.tsx
+++ frontend/components/alerts/AlertsDetail/index.tsx
@@ -7,6 +7,7 @@ import { AlertCauseIcon, AlertEffectIcon } from '@/components/alerts/AlertCauseE
 import AlertInformedEntity from '@/components/alerts/AlertInformedEntity';
 import Button from '@/components/common/Button';
 import { Section } from '@/components/layout/Section';
+import { Surface } from '@/components/layout/Surface';
 import { useAlertsContext } from '@/contexts/Alerts.context';
 import { Image } from '@mantine/core';
 import { IconArrowUpRight, IconExternalLink } from '@tabler/icons-react';
@@ -41,27 +42,39 @@ export default function Component({ alertId }: Props) {
 
 	return (
 		<>
-			<Section backButtonHref="/alerts" childrenWrapperStyles={styles.infoBar} heading={simplifiedAlertData?.title} withGap={false} withTopBorder={false} withChildrenNudge withChildrenPadding>
-				{simplifiedAlertData?.cause && <AlertCauseIcon cause={simplifiedAlertData.cause} withText />}
-				{simplifiedAlertData?.effect && <AlertEffectIcon effect={simplifiedAlertData.effect} withText />}
-				{simplifiedAlertData?.start_date && <AlertActivePeriodStart date={simplifiedAlertData.start_date} />}
-				{simplifiedAlertData?.end_date && <AlertActivePeriodEnd date={simplifiedAlertData.end_date} />}
-			</Section>
-			<Section childrenWrapperStyles={styles.infoBar} withGap={false} withTopPadding={false} withChildrenPadding>
-				{simplifiedAlertData?.informed_entity && simplifiedAlertData?.informed_entity.map((entity, index) => (
-					<AlertInformedEntity key={index} lineId={entity.lineId} />
-				))}
-			</Section>
-			<Section childrenWrapperStyles={styles.contentWrapper} withTopPadding={false} withChildrenNudge withChildrenPadding>
-				{simplifiedAlertData?.description && <p className={styles.description}>{simplifiedAlertData.description}</p>}
-				{simplifiedAlertData?.image_url && (
-					<>
-						<Image alt={simplifiedAlertData?.title} className={styles.image} src={simplifiedAlertData.image_url} />
-						<Button href={simplifiedAlertData.image_url} icon={<IconArrowUpRight size={16} />} label={t('open_full_image')} target="_blank" variant="pill" />
-					</>
+			<Surface>
+				<Section heading={simplifiedAlertData?.title} withBottomDivider withPadding>
+					<div className={styles.infoBar}>
+						{simplifiedAlertData?.cause && <AlertCauseIcon cause={simplifiedAlertData.cause} withText />}
+						{simplifiedAlertData?.effect && <AlertEffectIcon effect={simplifiedAlertData.effect} withText />}
+						{simplifiedAlertData?.start_date && <AlertActivePeriodStart date={simplifiedAlertData.start_date} />}
+						{simplifiedAlertData?.end_date && <AlertActivePeriodEnd date={simplifiedAlertData.end_date} />}
+					</div>
+				</Section>
+				{simplifiedAlertData?.informed_entity && (
+					<Section withPadding>
+						<div className={styles.infoBar}>
+							{simplifiedAlertData?.informed_entity.map((entity, index) => (
+								<AlertInformedEntity key={index} lineId={entity.lineId} />
+							))}
+						</div>
+					</Section>
 				)}
-				{simplifiedAlertData?.url && <Button href={simplifiedAlertData.url || '#'} icon={<IconExternalLink size={18} />} label={t('more_info')} />}
-			</Section>
+			</Surface>
+			<Surface>
+				<Section withPadding>
+					<div className={styles.contentWrapper}>
+						{simplifiedAlertData?.description && <p className={styles.description}>{simplifiedAlertData.description}</p>}
+						{simplifiedAlertData?.image_url && (
+							<>
+								<Image alt={simplifiedAlertData?.title} className={styles.image} src={simplifiedAlertData.image_url} />
+								<Button href={simplifiedAlertData.image_url} icon={<IconArrowUpRight size={16} />} label={t('open_full_image')} target="_blank" variant="pill" />
+							</>
+						)}
+						{simplifiedAlertData?.url && <Button href={simplifiedAlertData.url || '#'} icon={<IconExternalLink size={18} />} label={t('more_info')} />}
+					</div>
+				</Section>
+			</Surface>
 		</>
 	);
 
