85e1629ff27709a347bfba1a7a6fe0d53a6bea16
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-10-08 08:57:53 +0100

    Added app store links

--- frontend/components/footer/SocialIcons/index.tsx
+++ frontend/components/footer/SocialIcons/index.tsx
@@ -1,7 +1,7 @@
 /* * */
 
 import { Routes } from '@/utils/routes';
-import { IconBrandFacebook, IconBrandInstagram, IconBrandTwitter, IconBrandWhatsapp } from '@tabler/icons-react';
+import { IconBrandAndroid, IconBrandApple, IconBrandFacebook, IconBrandInstagram, IconBrandTwitter, IconBrandWhatsapp } from '@tabler/icons-react';
 import Link from 'next/link';
 
 import styles from './styles.module.css';
@@ -11,16 +11,23 @@ import styles from './styles.module.css';
 export default function Component() {
 	return (
 		<div className={styles.container}>
-			<Link className={`${styles.iconWtapper} ${styles.facebook}`} href={Routes.FACEBOOK} rel="noopener noreferrer" target="_blank">
+			<Link className={`${styles.iconWrapper} ${styles.apple}`} href={Routes.APPLE_PROD} rel="noopener noreferrer" target="_blank">
+				<IconBrandApple />
+			</Link>
+			<Link className={`${styles.iconWrapper} ${styles.android}`} href={Routes.ANDROID_PROD} rel="noopener noreferrer" target="_blank">
+				<IconBrandAndroid />
+			</Link>
+			<div className={styles.divider} />
+			<Link className={`${styles.iconWrapper} ${styles.facebook}`} href={Routes.FACEBOOK} rel="noopener noreferrer" target="_blank">
 				<IconBrandFacebook />
 			</Link>
-			<Link className={`${styles.iconWtapper} ${styles.instagram}`} href={Routes.INSTAGRAM} rel="noopener noreferrer" target="_blank">
+			<Link className={`${styles.iconWrapper} ${styles.instagram}`} href={Routes.INSTAGRAM} rel="noopener noreferrer" target="_blank">
 				<IconBrandInstagram />
 			</Link>
-			<Link className={`${styles.iconWtapper} ${styles.twitter}`} href={Routes.X} rel="noopener noreferrer" target="_blank">
+			<Link className={`${styles.iconWrapper} ${styles.twitter}`} href={Routes.X} rel="noopener noreferrer" target="_blank">
 				<IconBrandTwitter />
 			</Link>
-			<Link className={`${styles.iconWtapper} ${styles.whatsapp}`} href={Routes.WHATSAPP} rel="noopener noreferrer" target="_blank">
+			<Link className={`${styles.iconWrapper} ${styles.whatsapp}`} href={Routes.WHATSAPP} rel="noopener noreferrer" target="_blank">
 				<IconBrandWhatsapp />
 			</Link>
 		</div>
