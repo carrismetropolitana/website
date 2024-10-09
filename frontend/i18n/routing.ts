9e91c955c8e2546e1b80de0939885dd6f52ac560
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-10-09 11:14:57 +0100

    Updated packages

--- frontend/i18n/routing.ts
+++ frontend/i18n/routing.ts
@@ -6,13 +6,14 @@ import { createLocalizedPathnamesNavigation } from 'next-intl/navigation';
 import { defineRouting } from 'next-intl/routing';
 
 /* * */
+
 const intlPathnames = Object.entries(Routes).reduce(
 	(acc: Record<string, Record<string, string>>, [, value]) => {
 		if (typeof value === 'object' && 'route' in value && 'intl' in value) {
 			const translatedPathnames = value['intl'] as Record<string, string>;
 			acc[value['route']] = translatedPathnames;
 
-			// Check if any of the available locales is not in translatedPathnames
+			// Check if any of the available locales is in translatedPathnames
 			// If not, add the default locale
 			for (const localeCode of [...enabledLocaleCodes, ...enabledLocaleAlias]) {
 				if (!(localeCode in translatedPathnames)) {
