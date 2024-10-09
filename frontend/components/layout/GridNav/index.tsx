a9c331d156c9b6edba7e5ec983528f6796caf558
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-10-09 13:34:57 +0100

    Fix build error by moving file to TS

--- frontend/components/layout/GridNav/index.tsx
+++ frontend/components/layout/GridNav/index.tsx
@@ -6,7 +6,19 @@ import styles from './styles.module.css';
 
 /* * */
 
-export default function Component({ className = '', items = [] }) {
+interface Props {
+	className?: string
+	items: {
+		_id?: string
+		href?: string
+		icon?: React.ReactNode
+		label?: string
+	}[]
+}
+
+/* * */
+
+export default function Component({ className = '', items = [] }: Props) {
 	return (
 		<div className={`${styles.container} ${className}`}>
 			{items.map((item, index) => (
