110b57b625e4e217058189a63c6350e1e057443a
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-10-10 16:35:35 +0100

    Refactor Grid component to accept custom classNames

--- frontend/components/layout/Grid/index.tsx
+++ frontend/components/layout/Grid/index.tsx
@@ -6,15 +6,16 @@ import styles from './styles.module.css';
 
 interface Props {
 	children?: React.ReactNode
+	className?: string
 	columns?: 'a' | 'aab' | 'ab' | 'abb' | 'abc' | 'abcd'
 	withGap?: boolean
 }
 
 /* * */
 
-export function Grid({ children, columns = 'a', withGap }: Props) {
+export function Grid({ children, className, columns = 'a', withGap }: Props) {
 	return (
-		<div className={`${styles.container} ${styles[columns]} ${withGap && styles.withGap}`}>
+		<div className={`${styles.container} ${styles[columns]} ${withGap && styles.withGap} ${className}`}>
 			{children}
 		</div>
 	);
