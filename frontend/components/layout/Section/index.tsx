bd4fbd0b0fc4cbc4821036488eb4257f3d231526
Josue Monteiro <jusi.monteiro@protonmail.com>
2024-10-10 16:34:45 +0100

    Refactor Section component layout for enhanced styling and alignment
    Updated Section component layout to include a new 'right' element for improved styling and alignment within sections.
    - Adjusted CSS to implement a top section with flex layout and added padding

--- frontend/components/layout/Section/index.tsx
+++ frontend/components/layout/Section/index.tsx
@@ -7,6 +7,7 @@ import styles from './styles.module.css';
 interface Props {
 	children?: React.ReactNode
 	heading?: string
+	right?: React.ReactNode
 	subheading?: string
 	withBottomDivider?: boolean
 	withGap?: boolean
@@ -15,14 +16,17 @@ interface Props {
 
 /* * */
 
-export function Section({ children, heading, subheading, withBottomDivider, withGap, withPadding }: Props) {
+export function Section({ children, heading, right, subheading, withBottomDivider, withGap, withPadding }: Props) {
 	return (
 		<section className={`${styles.container} ${withBottomDivider && styles.withBottomDivider} ${withGap && styles.withGap} ${withPadding && styles.withPadding}`}>
 
 			{(heading || subheading) && (
-				<div className={styles.headingWrapper}>
-					{heading && <h1 className={styles.heading}>{heading}</h1>}
-					{subheading && <h6 className={styles.subheading}>{subheading}</h6>}
+				<div className={styles.topSection}>
+					<div className={styles.headingWrapper}>
+						{heading && <h1 className={styles.heading}>{heading}</h1>}
+						{subheading && <h6 className={styles.subheading}>{subheading}</h6>}
+					</div>
+					{right && <div className={styles.right}>{right}</div>}
 				</div>
 			)}
 
@@ -31,7 +35,6 @@ export function Section({ children, heading, subheading, withBottomDivider, with
 					{children}
 				</div>
 			)}
-
 		</section>
 	);
 }
