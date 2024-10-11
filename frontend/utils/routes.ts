61870e9a3942620f26e8e8827260294f60901cd4
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-10-11 18:27:56 +0100

    Fix API route

--- frontend/utils/routes.ts
+++ frontend/utils/routes.ts
@@ -188,7 +188,7 @@ export const Routes = Object.freeze({
 	...RoutesLostAndFound,
 	...RoutesFooter,
 
-	API: process.env.NEXT_PUBLIC_API_URL ?? 'https://api.cmet.pt',
+	API: process.env.NEXT_PUBLIC_API_URL ?? 'https://api.cmet.pt/v2',
 	API_ACCOUNTS: process.env.ACCOUNTS_API_URL ?? 'https:://accounts.carrismetropolitana.pt',
 	METRICS: {
 		intl: {
