6be7e15518e5fb25d29957330f7194108ada98da
Bruno Castelo <brunoalexcastelo@gmail.com>
2025-03-06 14:53:04 +0000

    Tweaks

--- frontend/components/stops/StopProbe/index.tsx
+++ frontend/components/stops/StopProbe/index.tsx
@@ -1,7 +1,9 @@
+'use client';
 /* * */
 
 import { StopProbeSection } from '@/components/stops/StopProbeSection';
 import { useTranslations } from 'next-intl';
+import { use, useEffect, useState } from 'react';
 
 /* * */
 
@@ -10,10 +12,13 @@ export function StopProbe() {
 
 	//
 	// A. Setup Variables
-
+	const [selectedStop, setSelectedStop] = useState('');
 	const t = useTranslations('stops.Probe');
-	const url = window.location.href;
-	const selectedStop = url.split('/').pop();
+	useEffect(() => {
+		if (typeof window === 'undefined') return;
+		const url = window.location.href;
+		setSelectedStop(url.split('/').pop() || '');
+	}, []);
 
 	//
 	// B. Render Components
