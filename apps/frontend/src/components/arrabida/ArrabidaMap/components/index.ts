f83b500d9bc3bd2a74e201f1d2902813019c3b72
Victor Micco <victor.oficial093@gmail.com>
2025-08-18 18:01:16 -0300

    Delete apps/frontend/src/components/arrabida/ArrabidaMap/components/index.ts
    remove unused file

--- apps/frontend/src/components/arrabida/ArrabidaMap/components/index.ts
+++
@@ -1,4 +0,0 @@
-/* * */
-
-// Note: Individual pin components and ArrabidaBeachPins/ArrabidaLineDisplay were removed
-// as they were unused. The project now uses DraggableAllPins with AllPins component.
