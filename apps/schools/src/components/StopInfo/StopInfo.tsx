d08e088f56cf4a5768be1a3c85a485db19c1cb77
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-09-03 15:49:31 +0100

    Update URLs

--- apps/schools/src/components/StopInfo/StopInfo.tsx
+++ apps/schools/src/components/StopInfo/StopInfo.tsx
@@ -1,10 +1,18 @@
 'use client';
 
-import LineDisplay from '@/components/LineDisplay/LineDisplay';
+/* * */
+
+import { LineDisplay } from '@/components/LineDisplay/LineDisplay';
+import { type ApiResponse } from '@carrismetropolitana/api-types/common';
+import { type Locality } from '@carrismetropolitana/api-types/locations';
+import { type Stop } from '@carrismetropolitana/api-types/network';
 import Link from 'next/link';
+import { useMemo } from 'react';
 import useSWR from 'swr';
 
 import styles from './StopInfo.module.css';
+
+/* * */
 
 export default function StopInfo({ index, stop_id }) {
 	//
@@ -12,22 +20,35 @@ export default function StopInfo({ index, stop_id }) {
 	//
 	// A. Fetch data
 
-	const { data: stopData } = useSWR(`https://api.carrismetropolitana.pt/stops/${stop_id}`);
+	const { data: allStopsData } = useSWR<Stop[]>('https://api.carrismetropolitana.pt/v2/stops');
+	const { data: allLocalitiesData } = useSWR<ApiResponse<Locality[]>>('https://api.carrismetropolitana.pt/v2/locations/localities');
 
 	//
-	// B. Render components
+	// B. Transform data
+
+	const stopData: Stop = useMemo(() => {
+		return allStopsData?.find(item => item.id === stop_id);
+	}, [allStopsData, stop_id, allLocalitiesData]);
+
+	const localityData: Locality = useMemo(() => {
+		if (allLocalitiesData?.status !== 'success') return;
+		return allLocalitiesData?.data?.find(item => item.id === stopData?.locality_id);
+	}, [allLocalitiesData, stopData]);
+
+	//
+	// C. Render components
 
 	return (
 		stopData
-		&& stopData.routes?.length > 0
+		&& stopData.pattern_ids?.length > 0
 		&& (
 			<div className={styles.container}>
 				<div className={styles.headerWrapper}>
 					{index && <div className={styles.stopIndex}>{index}</div>}
 					<div className={styles.header}>
-						<div className={styles.stopName}>{stopData.name}</div>
+						<div className={styles.stopName}>{stopData.long_name}</div>
 						<div className={styles.stopDetails}>
-							<div className={styles.stopLocation}>{stopData.locality}</div>
+							{localityData?.name && <div className={styles.stopLocation}>{localityData.name}</div>}
 							<Link className={styles.stopId} href={`https://carrismetropolitana.pt/stops/${stopData.id}`} target="_blank">
 								#
 								{stopData.id}
@@ -40,10 +61,11 @@ export default function StopInfo({ index, stop_id }) {
 				</div>
 
 				<div className={styles.linesList}>
-					{stopData.routes?.map(routeId => <LineDisplay key={routeId} route_id={routeId} />)}
+					{stopData.pattern_ids?.map(patternId => <LineDisplay key={patternId} patternId={patternId} />)}
 				</div>
 			</div>
 		)
-
 	);
+
+	//
 }
