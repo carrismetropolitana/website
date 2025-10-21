a87aaa1765a06ccb6de98b091368b9c6ce1797cb
Afonso Esteves <afonsomgesteves@gmail.com>
2025-10-21 17:20:56 +0100

    Update packages

--- shared/settings/src/public-variables.ts
+++ shared/settings/src/public-variables.ts
@@ -9,7 +9,7 @@ import { type Environment, getCurrentEnvironment } from '@/environment.js';
 export const PUBLIC_VARIABLES = Object.freeze({
 
 	api_url: {
-		development: 'http://api.carrismetropolitana.pt/v2',
+		development: 'http://127.0.0.1:5050',
 		production: 'https://api.carrismetropolitana.pt/v2',
 		staging: 'https://api.carrismetropolitana.pt/v2',
 	},
