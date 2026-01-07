84452006ccd257e7f10584f475073601f5c8ab6a
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-01-07 11:01:47 +0000

    Fix URL

---
+++ apps/frontend/src/app/(views)/(mini-apps)/quiz/page.tsx
@@ -0,0 +1,5 @@
+/* * */
+
+export default function Page() {
+	return <iframe src="https://storage.carrismetropolitana.pt/static/quiz" style={{ border: 'none', display: 'flex', height: '100vh', margin: 0, padding: 0, width: '100vw' }} />;
+}
