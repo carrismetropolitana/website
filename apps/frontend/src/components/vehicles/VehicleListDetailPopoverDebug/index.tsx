f83196edd28d0c6184b7a2991bb54ebfc6a61416
Bruno Castelo <brunoalexcastelo@gmail.com>
2026-01-08 17:28:05 +0000

    Refactor VehicleListDetailPopoverDebug to improve timestamp and delay display; streamline variable setup for clarity

--- apps/frontend/src/components/vehicles/VehicleListDetailPopoverDebug/index.tsx
+++ apps/frontend/src/components/vehicles/VehicleListDetailPopoverDebug/index.tsx
@@ -14,17 +14,33 @@ interface VehicleListDetailPopoverProps {
 }
 
 export default function VehicleListDetailPopoverDebug({ data }: VehicleListDetailPopoverProps) {
-	if (!data) return null;
+	//
+
+	//
+	// A. Setup variables
+
+	const delay = Math.floor(Date.now() / 1000) - data.timestamp;
+	const delayString = delay > 0 ? `+${delay} seconds` : `${delay} seconds`;
+	const timestampString = new Date(data.timestamp * 1000).toLocaleString();
+
+	//
+	// B. Render Components
 
 	return (
-		<div className={styles.container}>
-			<CopyBadge label={`Vehicle ID: ${data.id}`} value={data.id} />
-			<CopyBadge label={`Timestamp: ${new Date(data.timestamp * 1000).toLocaleString()}`} value={new Date(data.timestamp * 1000).toLocaleString()} />
-			<CopyBadge label={`Delay: ${Math.floor(Date.now() / 1000) - data.timestamp} seconds`} value={Math.floor(Date.now() / 1000) - data.timestamp} />
-			<CopyBadge label={`Trip ID: ${data.trip_id}`} value={data.trip_id} />
-			<CopyBadge label={`Status: ${data.current_status} : ${data.stop_id}`} value={data.current_status} />
-			<CopyBadge label={`Block ID: ${data.block_id}`} value={data.block_id} />
-			<CopyBadge label={`Shift ID: ${data.shift_id}`} value={data.shift_id} />
-		</div>
+		<>
+			{data && (
+				<div className={styles.container}>
+					<CopyBadge label={`Vehicle ID: ${data.id}`} value={data.id} />
+					<CopyBadge label={`Timestamp: ${timestampString}`} value={timestampString} />
+					<CopyBadge label={`Delay: ${delayString}`} value={delayString} />
+					<CopyBadge label={`Trip ID: ${data.trip_id}`} value={data.trip_id} />
+					<CopyBadge label={`Status: ${data.current_status} : ${data.stop_id}`} value={data.current_status} />
+					<CopyBadge label={`Block ID: ${data.block_id}`} value={data.block_id} />
+					<CopyBadge label={`Shift ID: ${data.shift_id}`} value={data.shift_id} />
+				</div>
+			)}
+		</>
 	);
+
+	//
 }
