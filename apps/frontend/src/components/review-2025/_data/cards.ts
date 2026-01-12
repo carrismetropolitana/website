7a720f8ac6b7efe4c5b20f8cc2c1b5cd9a3b4aa7
Jusi Monteiro <jusi.monteiro@protonmail.com>
2026-01-12 18:18:51 +0000

    Add Review 2025 card data structure and initial content

---
+++ apps/frontend/src/components/review-2025/_data/cards.ts
@@ -0,0 +1,79 @@
+/* * */
+
+import { OneOrTheOther } from '@tmlmobilidade/types';
+
+export const Review2025Badge = [
+	'PAX_TOTAL',
+	'RECORDES_DU',
+	'KMS_TOTAL',
+	'BRIDGES_TOTAL',
+	'TRIPS_TOTAL',
+	'LX_TOTAL',
+	'RECORDES_SAB',
+	'RECORDES_DOM',
+	'GROWTH',
+] as const;
+
+export interface Review2025CardSchema {
+	_group: string
+	_group_title?: string
+	_id: string
+	active_badges: typeof Review2025Badge[number][]
+	area: 1 | 2 | 3 | 4
+	badges: typeof Review2025Badge[number][]
+	color: string
+	content: {
+		content_group: Review2025CardSchemaContentGroup[]
+		description: string
+	}
+	header: string
+}
+
+export interface Review2025CardSchemaContentGroup {
+	items: OneOrTheOther<Review2025CardSchemaContentGroupBadgeComponent, Review2025CardSchemaContentGroupLines>[]
+	title?: string
+}
+
+export interface Review2025CardSchemaContentGroupBadgeComponent {
+	badge: typeof Review2025Badge[number]
+	description: string
+	title: string
+}
+
+export interface Review2025CardSchemaContentGroupLines {
+	line_id: string
+	line_name: string
+}
+
+/* * */
+
+export const allCardsData: Review2025CardSchema[] = [
+
+	//
+	// Group: "area_1"
+
+	{
+		_group: 'aml',
+		_group_title: 'AML',
+		_id: 'aml',
+		active_badges: ['PAX_TOTAL', 'KMS_TOTAL', 'TRIPS_TOTAL'],
+		area: 1,
+		badges: ['PAX_TOTAL', 'RECORDES_DU', 'KMS_TOTAL', 'BRIDGES_TOTAL', 'TRIPS_TOTAL', 'LX_TOTAL', 'GROWTH'],
+		color: '#FFDD00',
+		content: {
+			content_group: [
+				{
+					items: [
+						{
+							badge: 'PAX_TOTAL',
+							description: 'total de passageiros transportados em 2025',
+							title: 'Passageiros',
+						},
+					],
+				},
+			],
+			description: '',
+		},
+		header: 'header',
+	},
+];
