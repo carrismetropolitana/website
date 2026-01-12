90d2c338b0104876a3d186724f4f98b4fd7267e8
Jusi Monteiro <jusi.monteiro@protonmail.com>
2026-01-12 17:22:27 +0000

    Setup review 2025 Page

---
+++ apps/frontend/src/components/review-2025/Review2025Page/index.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+/* * */
+
+/* * */
+
+export function Review2025Page() {
+	//
+
+	//
+	// A. Setup variables
+
+	//
+	// B. Render components
+
+	return (
+		<>
+
+		</>
+	);
+
+	//
+}
