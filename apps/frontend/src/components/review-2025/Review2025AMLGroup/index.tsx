e82af7e622f24786cfc24050060116ead583ea9e
Jusi Monteiro <jusi.monteiro@protonmail.com>
2026-01-14 17:29:51 +0000

    Refactor Review2025AMLGroup and Review2025AreaGroup components to enhance layout with new Section wrappers; introduce styles for headings and subheadings, and improve grid structure for better content presentation.

--- apps/frontend/src/components/review-2025/Review2025AMLGroup/index.tsx
+++ apps/frontend/src/components/review-2025/Review2025AMLGroup/index.tsx
@@ -1,8 +1,11 @@
 /* * */
 
 import { Grid } from '@/components/layout/Grid';
+import { Section } from '@/components/layout/Section';
 import { Surface } from '@/components/layout/Surface';
 import { amlData } from '@/components/review-2025/_data/cards';
+
+import styles from './styles.module.css';
 
 import Review2025Card from '../Review2025Card';
 
@@ -12,9 +15,17 @@ export function Review2025AMLGroup() {
 	//
 	return (
 		<Surface forceOverflow>
-			<Grid columns="abc" withGap>
-				{amlData.map((data, index) => <Review2025Card key={index} data={data} />)}
-			</Grid>
+			<Section withPadding="desktop" withGap>
+				<div className={styles.headingWrapper}>
+					<h2 className={styles.heading}>Área Metropolitana de Lisboa</h2>
+					<h5 className={styles.subheading}>Evolução da operação na Área Metropolitana de Lisboa em 2025</h5>
+				</div>
+			</Section>
+			<Section withPadding="desktop" withGap>
+				<Grid columns="ab" withGap>
+					{amlData.map((data, index) => <Review2025Card key={index} data={data} />)}
+				</Grid>
+			</Section>
 		</Surface>
 	);
 	//
