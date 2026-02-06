6b3d3b1174aa0318ff7101b06b9c297d816493d2
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-06 18:26:28 +0000

    Add triple click to the logo

--- apps/frontend/src/components/header/Logo/index.tsx
+++ apps/frontend/src/components/header/Logo/index.tsx
@@ -4,20 +4,60 @@
 
 import { LottiePlayer } from '@/components/common/LottiePlayer';
 import { ThemeSwitch } from '@/components/responsive/ThemeSwitch';
-import Link from 'next/link';
+import { useDebugContext } from '@/contexts/Debug.context';
+import { useRef } from 'react';
 
 import styles from './styles.module.css';
 
 /* * */
 
 export function Logo() {
+	//
+
+	//
+	// A. Setup variables
+
+	const debugContext = useDebugContext();
+
+	const clickCountRef = useRef(0);
+	const clickTimeoutRef = useRef<null | ReturnType<typeof setTimeout>>(null);
+
+	//
+	// B. Handle actions
+
+	const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		// Increment click count
+		clickCountRef.current += 1;
+		// Clear previous timeout if it exists
+		if (clickTimeoutRef.current) clearTimeout(clickTimeoutRef.current);
+		// If click count reaches 3, toggle debug mode
+		if (clickCountRef.current === 3) {
+			event.stopPropagation();
+			if (clickTimeoutRef.current) clearTimeout(clickTimeoutRef.current);
+			clickCountRef.current = 0;
+			debugContext.actions.toggleDebugMode();
+			return;
+		}
+		// Set timeout to reset click count after 400ms
+		// and navigate to home page if it was a single click
+		clickTimeoutRef.current = setTimeout(() => {
+			if (clickCountRef.current === 1) {
+				window.location.href = '/';
+			}
+			clickCountRef.current = 0;
+		}, 400);
+	};
+
+	//
+	// C. Render components
+
 	return (
-		<Link aria-label="Carris Metropolitana" className={styles.container} href="/">
+		<div aria-label="Carris Metropolitana" className={styles.container} onClick={handleClick}>
 			<ThemeSwitch
 				dark={<LottiePlayer path="/assets/header/zume/zume-dark.json" style={{ height: 70, width: 150 }} loop play />}
 				light={<LottiePlayer path="/assets/header/zume/zume-light.json" style={{ height: 70, width: 150 }} loop play />}
 			/>
-		</Link>
+		</div>
 	);
 }
 
