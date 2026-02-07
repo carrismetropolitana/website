ccf17381331e164802d1e16d1271b4c32ba890ba
Movies22 <github.movies22@gmail.com>
2026-02-07 15:05:54 +0000

    Add embed to Arrabida365 page

--- apps/frontend/src/app/(views)/(website)/arrabida365/page.tsx
+++ apps/frontend/src/app/(views)/(website)/arrabida365/page.tsx
@@ -3,7 +3,16 @@ import { ArrabidaPage } from '@/components/arrabida/ArrabidaPage';
 import { AlertsContextProvider } from '@/contexts/Alerts.context';
 import { LinesDetailContextProvider } from '@/contexts/LinesDetail.context';
 import { LinesListContextProvider } from '@/contexts/LinesList.context';
+import { type Metadata } from 'next';
 
+/* * */
+export const metadata: Metadata = {
+	description: 'Este projeto tem como objetivo promover a mobilidade sustentável na Serra da Arrábida.',
+	openGraph: {
+		images: [{ url: '/assets/arrabidas/arrabida_365_map.png' }],
+	},
+	title: 'CMetropolitana | Arrabida365',
+};
 /* * */
 
 export default function Page() {
