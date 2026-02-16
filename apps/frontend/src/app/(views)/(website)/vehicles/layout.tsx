c3f5cda7a0d90da4a067753ed4937999bb489c39
Movies22 <github.movies22@gmail.com>
2026-02-16 16:09:13 +0000

    Update layout.tsx

--- apps/frontend/src/app/(views)/(website)/vehicles/layout.tsx
+++ apps/frontend/src/app/(views)/(website)/vehicles/layout.tsx
@@ -7,7 +7,7 @@ import { type Metadata } from 'next';
 
 export const metadata: Metadata = {
 	description: 'Explore todos os veículos da CMetropolitana em tempo real.',
-	title: 'CMetropolitana | Frota',
+	title: 'CMetropolitana | Veículos a circular',
 };
 
 /* * */
