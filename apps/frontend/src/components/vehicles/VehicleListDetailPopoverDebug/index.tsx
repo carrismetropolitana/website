dac2fffa36146f473165168d475c44f2061a990d
Bruno Castelo <brunoalexcastelo@gmail.com>
2026-03-02 15:55:10 +0000

    Refactor VehicleListDetailPopoverDebug component by removing unwanted timestamp and delay variables

--- apps/frontend/src/components/vehicles/VehicleListDetailPopoverDebug/index.tsx
+++ apps/frontend/src/components/vehicles/VehicleListDetailPopoverDebug/index.tsx
@@ -17,13 +17,6 @@ export function VehicleListDetailPopoverDebug({ data }: VehicleListDetailPopover
 	//
 
 	//
-	// A. Setup variables
-
-	const delay = Math.floor(Date.now() / 1000) - data.timestamp;
-	const delayString = delay > 0 ? `+${delay} seconds` : `${delay} seconds`;
-	const timestampString = new Date(data.timestamp * 1000).toLocaleString();
-
-	//
 	// B. Render Components
 
 	return (
@@ -31,12 +24,7 @@ export function VehicleListDetailPopoverDebug({ data }: VehicleListDetailPopover
 			{data && (
 				<div className={styles.container}>
 					<CopyBadge label={`Vehicle ID: ${data.id}`} value={data.id} />
-					<CopyBadge label={`Timestamp: ${timestampString}`} value={timestampString} />
-					<CopyBadge label={`Delay: ${delayString}`} value={delayString} />
 					<CopyBadge label={`Trip ID: ${data.trip_id}`} value={data.trip_id} />
-					<CopyBadge label={`Status: ${data.current_status} : ${data.stop_id}`} value={data.current_status} />
-					<CopyBadge label={`Block ID: ${data.block_id}`} value={data.block_id} />
-					<CopyBadge label={`Shift ID: ${data.shift_id}`} value={data.shift_id} />
 				</div>
 			)}
 		</>
