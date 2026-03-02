1ac8a2929680cd30eb45f50c424adf4cb2290971
Bruno Castelo <brunoalexcastelo@gmail.com>
2026-03-02 14:43:52 +0000

    Refactor PipsStopDetail and PipsStopsList components: updated stopId handling to ensure string conversion, improved rendering structure by wrapping components in Surface and Section for better layout, and enhanced readability of the code.

--- apps/frontend/src/components/pips-survey/PipsStopDetail/index.tsx
+++ apps/frontend/src/components/pips-survey/PipsStopDetail/index.tsx
@@ -26,28 +26,28 @@ export function PipsStopDetail({ stopId }: PipsStopDetailProps) {
 	//
 	// B. Handle actions
 
-	const stop = stopsContext.actions.getStopById(stopId);
+	const stop = stopsContext.actions.getStopById(String(stopId));
 
 	//
 	// B. Handle actions
 
 	const handleSelectAnswer = () => {
-		pipsContext.actions.selectStop(stopId);
+		pipsContext.actions.selectStop(String(stopId));
 	};
 
 	//
 	// C. Render components
 
 	if (stop) {
-		<div className={styles.container} onClick={handleSelectAnswer}>
-			<p className={styles.stopName}>{stop?.long_name}</p>
-			<p className={styles.stopId}>{stop?.id}</p>
-		</div>;
+		return (
+			<div className={styles.container} onClick={handleSelectAnswer}>
+				<p className={styles.stopName}>{stop.long_name}</p>
+				<p className={styles.stopId}>{stop.id}</p>
+			</div>
+		);
 	}
 
 	if (pipsContext.flags.is_loading) {
 		return <Loader visible />;
 	}
-
-	//
 }
--- apps/frontend/src/components/pips-survey/PipsStopsList/index.tsx
+++ apps/frontend/src/components/pips-survey/PipsStopsList/index.tsx
@@ -2,6 +2,8 @@
 
 import { Loader } from '@/components/common/Loader';
 import { NoDataLabel } from '@/components/layout/NoDataLabel';
+import { Section } from '@/components/layout/Section';
+import { Surface } from '@/components/layout/Surface';
 import { PipsStopDetail } from '@/components/pips-survey/PipsStopDetail';
 import { usePipsContext } from '@/contexts/Pips.context';
 import { useTranslations } from 'next-intl';
@@ -23,10 +25,14 @@ export function PipsStopsList() {
 	// B. Render components
 
 	return (
-		<div className={styles.container}>
-			<p>{t('question')}</p>
-			<div className={styles.answersGrid}>{pipsContext.data.pipData ? pipsContext.data.pipData.stop_ids?.length > 0 ? pipsContext.data.pipData.stop_ids.map(item => <PipsStopDetail key={item} stopId={item} />) : <NoDataLabel text={t('no_data')} /> : <Loader visible />}</div>
-		</div>
+		<Surface>
+			<Section withGap withPadding>
+				<div className={styles.container}>
+					<p>{t('question')}</p>
+					<div className={styles.answersGrid}>{pipsContext.data.pipData ? pipsContext.data.pipData.stop_ids?.length > 0 ? pipsContext.data.pipData.stop_ids.map(item => <PipsStopDetail key={item} stopId={item} />) : <NoDataLabel text={t('no_data')} /> : <Loader visible />}</div>
+				</div>
+			</Section>
+		</Surface>
 	);
 
 	//
