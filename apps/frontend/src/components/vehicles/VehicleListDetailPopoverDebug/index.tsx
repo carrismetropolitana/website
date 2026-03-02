856821377b5634c1dd496256c49ee0c8bf9c80f0
Bruno Castelo <brunoalexcastelo@gmail.com>
2026-03-02 16:14:02 +0000

    Add expanded view to VehicleListDetailPopoverDebug component with additional vehicle data

--- apps/frontend/src/components/vehicles/VehicleListDetailPopoverDebug/index.tsx
+++ apps/frontend/src/components/vehicles/VehicleListDetailPopoverDebug/index.tsx
@@ -4,6 +4,8 @@
 
 import { CopyBadge } from '@/components/common/CopyBadge';
 import { Vehicle } from '@carrismetropolitana/api-types/vehicles';
+import { IconEyeMinus, IconEyePlus } from '@tabler/icons-react';
+import { useState } from 'react';
 
 import styles from './styles.module.css';
 
@@ -17,18 +19,39 @@ export function VehicleListDetailPopoverDebug({ data }: VehicleListDetailPopover
 	//
 
 	//
-	// B. Render Components
+	// A. Setup variables
 
-	return (
-		<>
-			{data && (
-				<div className={styles.container}>
-					<CopyBadge label={`Vehicle ID: ${data.id}`} value={data.id} />
-					<CopyBadge label={`Trip ID: ${data.trip_id}`} value={data.trip_id} />
-				</div>
-			)}
-		</>
-	);
+	const [expanded, setExpanded] = useState(false);
+
+	const delay = data ? Math.floor(Date.now() / 1000) - data.timestamp : 0;
+	const delayString = delay > 0 ? `+${delay} seconds` : `${delay} seconds`;
+	const timestampString = data ? new Date(data.timestamp * 1000).toLocaleString() : '';
 
 	//
+	// B. Render Components
+
+	if (!data) return null;
+
+	const renderExtraData = () => {
+		return (
+			<>
+				<CopyBadge label={`Timestamp: ${timestampString}`} value={timestampString} />
+				<CopyBadge label={`Delay: ${delayString}`} value={delayString} />
+				<CopyBadge label={`Status: ${data.current_status} : ${data.stop_id}`} value={data.current_status} />
+				<CopyBadge label={`Block ID: ${data.block_id}`} value={data.block_id} />
+				<CopyBadge label={`Shift ID: ${data.shift_id}`} value={data.shift_id} />
+			</>
+		);
+	};
+
+	return (
+		<div className={styles.container}>
+			<CopyBadge label={`Vehicle ID: ${data.id}`} value={data.id} />
+			<CopyBadge label={`Trip ID: ${data.trip_id}`} value={data.trip_id} />
+
+			{expanded ? <IconEyeMinus className={styles.expandButton} color="var(--color-debug)" onClick={() => setExpanded(prev => !prev)} size={20} /> : <IconEyePlus className={styles.expandButton} color="var(--color-debug)" onClick={() => setExpanded(prev => !prev)} size={20} />}
+
+			{expanded && renderExtraData()}
+		</div>
+	);
 }
