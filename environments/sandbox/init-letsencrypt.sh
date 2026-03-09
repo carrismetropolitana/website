d399f6aaac08ff7a368e644a59765d1e5f03fd42
jusimen <jusi.monteiro@protonmail.com>
2026-03-09 11:26:35 +0000

    fix: update primary domain in Let's Encrypt initialization script for sandbox environment

--- environments/sandbox/init-letsencrypt.sh
+++ environments/sandbox/init-letsencrypt.sh
@@ -7,7 +7,7 @@
 email="carrismetropolitana@gmail.com"
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
-primary_domain=staging.carrismetropolitana.pt
+primary_domain=sandbox.carrismetropolitana.pt
 
 
 # # #
