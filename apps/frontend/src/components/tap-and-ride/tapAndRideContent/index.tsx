5fd8ec2223dd95c9324f528c031d281bc63c3c1a
Bruno Castelo <brunoalexcastelo@gmail.com>
2026-03-26 12:24:50 +0000

    Enhancement - Update TapAndRideContent component to include multiple sections for improved structure and organization

--- apps/frontend/src/components/tap-and-ride/tapAndRideContent/index.tsx
+++ apps/frontend/src/components/tap-and-ride/tapAndRideContent/index.tsx
@@ -2,13 +2,27 @@
 /* * */
 
 import { TapAndRideSection1 } from '@/components/tap-and-ride/tapAndRideSection1';
+import { TapAndRideSection2 } from '@/components/tap-and-ride/tapAndRideSection2';
+import { TapAndRideSection3 } from '@/components/tap-and-ride/tapAndRideSection3';
+import { TapAndRideSection4 } from '@/components/tap-and-ride/tapAndRideSection4';
+import { TapAndRideSection5 } from '@/components/tap-and-ride/tapAndRideSection5';
+import { TapAndRideSectionFaqs } from '@/components/tap-and-ride/tapAndRideSectionFaqs';
+
+import styles from './styles.module.css';
 
 /* * */
 
 export function TapAndRideContent() {
 	return (
-		<div className={styles.contentRow}>
-			<TapAndRideSection1 />
+		<>
+			<div className={styles.contentRow}>
+				<TapAndRideSection1 />
+				<TapAndRideSection2 />
+				<TapAndRideSection3 />
+				<TapAndRideSection4 />
+				<TapAndRideSection5 />
+				<TapAndRideSectionFaqs />
+			</div>
 		</>
 	);
 }
---
+++ apps/frontend/src/components/tap-and-ride/tapAndRideSectionFaqs/index.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { Accordion, AccordionControl, AccordionItem, AccordionPanel } from '@mantine/core';
+/* * */
+
+import { Section } from '@/components/layout/Section';
+import { Image } from '@mantine/core';
+import { useTranslations } from 'next-intl';
+
+import styles from './styles.module.css';
+
+import { tapAndRideData } from '../_data/data';
+
+/* * */
+
+export function TapAndRideSectionFaqs() {
+	//
+
+	//
+	// A. Setup variables
+
+	const t = useTranslations('tap-and-ride');
+	const data = tapAndRideData;
+
+	//
+	// B. Render components
+
+	const renderAccordionItem = (item: { content: string, id: string, title: string }) => {
+		return (
+			<AccordionItem key={item.id} value={item.id}>
+				<AccordionControl><span className={styles.accordionControl}>{t(item.title)}</span></AccordionControl>
+				<AccordionPanel><div>{t(item.content)}</div></AccordionPanel>
+			</AccordionItem>
+		);
+	};
+
+	return (
+		<Section heading={t('heading')} subheading={t('subheading')} withPadding>
+			<Accordion className={styles.accordion}>
+				<Image alt="Tap and Ride" src="/assets/tap-and-ride/what_is_banner.svg" />
+				{data.map(item => renderAccordionItem(item))}
+			</Accordion>
+		</Section>
+	);
+}
