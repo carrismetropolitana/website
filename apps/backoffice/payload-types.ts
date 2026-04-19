b49bcf63f0747bf675185389496dfbf19208cb0e
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-04-19 19:10:57 +0100

    Updated packages

--- apps/backoffice/payload-types.ts
+++ apps/backoffice/payload-types.ts
@@ -112,6 +112,9 @@ export interface Config {
     settings: SettingsSelect<false> | SettingsSelect<true>;
   };
   locale: null;
+  widgets: {
+    collections: CollectionsWidget;
+  };
   user: User;
   jobs: {
     tasks: unknown;
@@ -844,6 +847,16 @@ export interface SettingsSelect<T extends boolean = true> {
   updatedAt?: T;
   createdAt?: T;
   globalType?: T;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "collections_widget".
+ */
+export interface CollectionsWidget {
+  data?: {
+    [k: string]: unknown;
+  };
+  width: 'full';
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
