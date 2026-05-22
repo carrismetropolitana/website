17408723e47f4b2f661a82c3867abc04ee525edc
Bruno Castelo <brunoalexcastelo@gmail.com>
2026-05-22 12:46:42 +0100

    Refactor - Remove carousel navigation controls from GalleryFieldPreview component

--- apps/backoffice/src/components/GalleryFieldPreview/index.tsx
+++ apps/backoffice/src/components/GalleryFieldPreview/index.tsx
@@ -49,54 +49,22 @@ export function GalleryFieldPreview() {
 	}, [images.length, isCarousel]);
 
 	//
-	// C. Handle Actions
-
-	const goPrev = () => setActiveIndex(i => (i <= 0 ? images.length - 1 : i - 1));
-	const goNext = () => setActiveIndex(i => (i >= images.length - 1 ? 0 : i + 1));
-
-	//
 	// D. Render components
 
 	if (!value?.length) return null;
 	if (!images.length) return <div className={styles.loadingContainer}>Loading...</div>;
 
-	if (!isCarousel) {
-		return (
-			<div className={styles.galleryContainer}>
-				{images.map((img, i) => (
-					<div key={img.id ?? i} className={styles.imageContainer}>
-						<img
-							alt={img.filename ?? `Image ${i + 1}`}
-							className={styles.image}
-							src={img.thumbnailURL ?? img.url}
-						/>
-					</div>
-				))}
-			</div>
-		);
-	}
-
 	return (
-		<div className={styles.carouselContainer}>
-			<div className={styles.carouselViewport}>
-				<div className={styles.imageContainer}>
+		<div className={styles.galleryContainer}>
+			{images.map((img, i) => (
+				<div key={img.id ?? i} className={styles.imageContainer}>
 					<img
-						alt={activeImage?.filename ?? `Image ${activeIndex + 1}`}
+						alt={img.filename ?? `Image ${i + 1}`}
 						className={styles.image}
-						src={activeImage?.thumbnailURL || activeImage?.url}
+						src={img.thumbnailURL ?? img.url}
 					/>
 				</div>
-			</div>
-			{images.length > 1 && (
-				<div className={styles.carouselControls}>
-					<button className={styles.carouselButton} onClick={goPrev}>
-						<IconChevronLeft size={16} />
-					</button>
-					<button className={styles.carouselButton} onClick={goNext}>
-						<IconChevronRight size={16} />
-					</button>
-				</div>
-			)}
+			))}
 		</div>
 	);
 
