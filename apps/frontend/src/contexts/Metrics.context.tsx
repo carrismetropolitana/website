e1802e38aea0021d466f86f63c2b01199945e569
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-06 03:17:55 +0100

    chore: update Metrics context imports for type consistency
    - Replaced imports from @tmlmobilidade/types with @tmlmobilidade/go-types-performance in Metrics.context.tsx to align with the new type definitions.
    - Ensured consistency in type handling across the Metrics context for improved data management.

--- apps/frontend/src/contexts/Metrics.context.tsx
+++ apps/frontend/src/contexts/Metrics.context.tsx
@@ -5,8 +5,8 @@
 import { formatDay, formatMonth } from '@/utils/formatDates';
 import { TopDemandLinesByAgency } from '@carrismetropolitana/api-types/metrics';
 import { getPublicVariable } from '@carrismetropolitana/website-shared-settings';
+import { type DemandByAgencyByDay, type DemandByAgencyByMonth, type DemandByLineByDay, type TopDemandByAgency } from '@tmlmobilidade/go-types-performance';
 import { Dates } from '@tmlmobilidade/go-utils-dates';
-import { type DemandByAgencyByDay, type DemandByAgencyByMonth, type DemandByLineByDay, type TopDemandByAgency } from '@tmlmobilidade/types';
 import { DateTime } from 'luxon';
 import { useTranslations } from 'next-intl';
 import { createContext, useContext, useMemo, useState } from 'react';
