c8e096c7a562db806e98e35243e7538cc81a2dc6
João de Vasconcelos <10943992+joao-vasconcelos@users.noreply.github.com>
2026-09-06 01:59:00 +0100

    chore: update PathWaypointTimetable and createTimetable for operational date handling
    - Modified PathWaypointTimetable to utilize OperationalDateInt for improved date management.
    - Updated createTimetable function to replace HubPattern and HubRoute types with their V1 API counterparts, enhancing type consistency.
    - Adjusted data handling logic to ensure accurate processing of operational dates.

--- apps/frontend/src/components/lines/PathWaypointTimetable/index.tsx
+++ apps/frontend/src/components/lines/PathWaypointTimetable/index.tsx
@@ -7,6 +7,7 @@ import { useLinesDetailContext } from '@/contexts/LinesDetail.context';
 import { useOperationalDateContext } from '@/contexts/OperationalDate.context';
 import { type Timetable } from '@/types/timetables.types';
 import { createTimetable } from '@/utils/create-timetable';
+import { OperationalDateInt } from '@tmlmobilidade/go-types-shared';
 import { Dates } from '@tmlmobilidade/go-utils-dates';
 import { useTranslations } from 'next-intl';
 import { useMemo } from 'react';
@@ -31,14 +32,14 @@ export function PathWaypointTimetable() {
 	//
 	// B. Transform data
 
-	const timetableData = useMemo<null | string | Timetable>(() => {
+	const timetableData = useMemo<null | OperationalDateInt | Timetable>(() => {
 		// Setup variables
 		const activePatternGroup = linesDetailContext.data.active_pattern;
 		const secondaryPatternGroups = linesDetailContext.data.valid_patterns?.filter(patternGroup => patternGroup.version_id !== activePatternGroup?.version_id) || [];
 		const mentionedRoutes = linesDetailContext.data.routes;
 		const selectedStopId = linesDetailContext.data.active_waypoint?.stop_id;
 		const selectedStopSequence = linesDetailContext.data.active_waypoint?.stop_sequence;
-		const selectedOperationalDate = operationalDateContext.data.selected_date?.operational_date;
+		const selectedOperationalDate = operationalDateContext.data.selected_date?.operational_date_int;
 		// Check if all these variables are defined
 		if (!activePatternGroup || !mentionedRoutes || !selectedStopId || selectedStopSequence === undefined || !selectedOperationalDate) {
 			return null;
@@ -48,9 +49,9 @@ export function PathWaypointTimetable() {
 		if (!activePatternGroup.valid_on.includes(selectedOperationalDate)) {
 			// Find the closest valid date
 			return activePatternGroup.valid_on.reduce((acc, curr) => {
-				if (selectedOperationalDate <= curr && (acc === '' || curr < acc)) return curr;
+				if (selectedOperationalDate <= curr && (acc === null || curr < acc)) return curr;
 				return acc;
-			}, '');
+			}, null);
 		}
 
 		// Check if the user has enabled complex schedules
@@ -80,8 +81,8 @@ export function PathWaypointTimetable() {
 		);
 	}
 
-	if (typeof timetableData === 'string') {
-		const nextDate = timetableData && Dates.fromOperationalDate(timetableData, 'Europe/Lisbon').js_date;
+	if (typeof timetableData === 'number') {
+		const nextDate = timetableData && Dates.fromOperationalDateInt(timetableData, 'Europe/Lisbon').js_date;
 		return (
 			<div className={styles.container}>
 				<p className={styles.noData}>{t('no_data')}</p>
--- apps/frontend/src/utils/create-timetable.ts
+++ apps/frontend/src/utils/create-timetable.ts
@@ -1,12 +1,12 @@
 /* * */
 
 import { type Timetable } from '@/types/timetables.types';
-import { type HubPattern, type HubRoute } from '@tmlmobilidade/go-types-hub';
+import { type HubV1ApiPattern, type HubV1ApiRoute } from '@tmlmobilidade/go-types-hub';
+import { type OperationalDateInt } from '@tmlmobilidade/go-types-shared';
 
 /**
  * This function creates a timetable for a set of patterns of a line, for a specific stop and date.
-  *Please explore the Timetable type to understand the structure of the resulting object.*
- *
+ * Please explore the Timetable type to understand the structure of the resulting object.*
  * @param primaryPatternGroup The primary pattern group of the timetable.
  * @param secondaryPatternGroups Other patterns used to extend the timetable with more data.
  * @param mentionedRoutes Parent Routes for the used patterns, used to display the route name in the exceptions.
@@ -16,7 +16,7 @@ import { type HubPattern, type HubRoute } from '@tmlmobilidade/go-types-hub';
  * @returns The timetable for the given patterns and stop.
  */
 
-export function createTimetable(primaryPatternGroup: HubPattern, secondaryPatternGroups: HubPattern[], mentionedRoutes: HubRoute[], stopId: string, stopSequence: number, operationalDate: string): Timetable {
+export function createTimetable(primaryPatternGroup: HubV1ApiPattern, secondaryPatternGroups: HubV1ApiPattern[], mentionedRoutes: HubV1ApiRoute[], stopId: string, stopSequence: number, operationalDate: OperationalDateInt): Timetable {
 //
 
 	// 1.
@@ -31,7 +31,7 @@ export function createTimetable(primaryPatternGroup: HubPattern, secondaryPatter
 	// 2.
 	// Extract the currently valid Pattern Group from the primary and secondary patterns.
 	// To check if a pattern is valid for the given date, we need to check if the date is included in the pattern's dates array.
-	const validSecondaryPatternGroups: HubPattern[] = secondaryPatternGroups.flat().filter(patternGroup => patternGroup.valid_on.includes(operationalDate) && patternGroup.direction_id === primaryPatternGroup.direction_id);
+	const validSecondaryPatternGroups: HubV1ApiPattern[] = secondaryPatternGroups.flat().filter(patternGroup => patternGroup.valid_on.includes(operationalDate) && patternGroup.direction_id === primaryPatternGroup.direction_id);
 
 	// 3.
 	// Create the timetable for the primary pattern first
